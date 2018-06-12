be4e489ccc975cac995122b6d5905af7563b34ca
demotto <huyifan_zju@163.com>
2018-06-12 19:47:01 +0800

    bugfix: dbutil get connection close connection when execute test sql failed

--- flinkx-rdb/src/main/java/com/dtstack/flinkx/rdb/util/DBUtil.java
+++ flinkx-rdb/src/main/java/com/dtstack/flinkx/rdb/util/DBUtil.java
@@ -57,24 +57,31 @@ public class DBUtil {
     }
 
     public static Connection getConnection(String url, String username, String password) throws SQLException {
-        Connection dbConn = getConnectionInternal(url, username, password);
-        boolean failed = true;
-        if (url.startsWith("jdbc:mysql")) {
-            for(int i = 0; i < MAX_RETRY_TIMES && failed; ++i) {
+        if (!url.startsWith("jdbc:mysql")) {
+            return getConnectionInternal(url, username, password);
+        } else {
+            boolean failed = true;
+            Connection dbConn = null;
+            for (int i = 0; i < MAX_RETRY_TIMES && failed; ++i) {
                 try {
+                    dbConn = getConnectionInternal(url, username, password);
                     dbConn.createStatement().execute("select 111");
                     failed = false;
-                } catch(SQLException e) {
-                    if(i == MAX_RETRY_TIMES) {
+                } catch (Exception e) {
+                    if (dbConn != null) {
+                        dbConn.close();
+                    }
+
+                    if (i == MAX_RETRY_TIMES) {
                         throw e;
                     } else {
                         SysUtil.sleep(3000);
                     }
                 }
             }
-        }
 
-        return dbConn;
+            return dbConn;
+        }
     }
 
 
