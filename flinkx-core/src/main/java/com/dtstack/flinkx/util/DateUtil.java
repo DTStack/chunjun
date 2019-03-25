1d78f67343977a40b97da9b761f8ac017dba6d66
jiangbo <jiangbo@dtstack.com>
2019-03-25 10:14:39 +0800

    修复Date类型转Timestamp报错

--- flinkx-core/src/main/java/com/dtstack/flinkx/util/DateUtil.java
+++ flinkx-core/src/main/java/com/dtstack/flinkx/util/DateUtil.java
@@ -123,7 +123,7 @@ public class DateUtil {
             Long rawData = (Long) column;
             return new java.sql.Timestamp(getMillSecond(rawData.toString()));
         } else if (column instanceof java.sql.Date) {
-            return (java.sql.Timestamp) column;
+            return new java.sql.Timestamp(((java.sql.Date) column).getTime());
         } else if(column instanceof Timestamp) {
             return (Timestamp) column;
         } else if(column instanceof Date) {
