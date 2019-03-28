53151d21fc2d052c04367df5d5c5d968e7dd2d06
toutian <toutian@dtstack.com>
2019-03-28 21:45:09 +0800

    mill

--- flinkx-core/src/main/java/com/dtstack/flinkx/metrics/InputMetric.java
+++ flinkx-core/src/main/java/com/dtstack/flinkx/metrics/InputMetric.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -93,7 +93,7 @@ public class InputMetric {
             long schedulePeriod = (long) periodField.get(scheduledFutureTask);
             long schedulePeriodMill = -1 * new FiniteDuration(schedulePeriod, TimeUnit.NANOSECONDS).toMillis();
 
-            LOG.info("InputMetric.scheduledFutureTask.schedulePeriodSecond(TimeUnit.MILLISECONDS):{} ...", schedulePeriodMill);
+            LOG.info("InputMetric.scheduledFutureTask.schedulePeriodMill:{} ...", schedulePeriodMill);
 
             if (schedulePeriodMill > DEFAULT_PERIOD_MILLISECONDS) {
                 this.delayPeriodMill = schedulePeriodMill * 2;
@@ -102,6 +102,6 @@ public class InputMetric {
             LOG.error("{}", e);
         }
 
-        LOG.info("InputMetric.period:{} ...", delayPeriodMill);
+        LOG.info("InputMetric.delayPeriodMill:{} ...", delayPeriodMill);
     }
 }
