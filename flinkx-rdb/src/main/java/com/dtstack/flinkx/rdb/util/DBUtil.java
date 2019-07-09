7d6c1590adc6522922bce2e13cb12f0fff38f0b6
jiangbo <jiangbo@dtstack.com>
2019-07-09 17:05:16 +0800

    fix gbase url参数添加

--- flinkx-rdb/src/main/java/com/dtstack/flinkx/rdb/util/DBUtil.java
+++ flinkx-rdb/src/main/java/com/dtstack/flinkx/rdb/util/DBUtil.java
@@ -17,6 +17,7 @@
  */
 package com.dtstack.flinkx.rdb.util;
 
+import com.dtstack.flinkx.constants.PluginNameConstrant;
 import com.dtstack.flinkx.enums.ColumnType;
 import com.dtstack.flinkx.enums.EDatabaseType;
 import com.dtstack.flinkx.rdb.DatabaseInterface;
@@ -594,9 +595,11 @@ public class DBUtil {
     }
 
     public static String formatJdbcUrl(String pluginName,String dbUrl){
-        if(pluginName.equalsIgnoreCase("mysqlreader")
-                || pluginName.equalsIgnoreCase("mysqldreader")
-                || pluginName.equalsIgnoreCase("postgresqlreader")){
+        if(pluginName.equalsIgnoreCase(PluginNameConstrant.MYSQL_READER)
+                || pluginName.equalsIgnoreCase(PluginNameConstrant.MYSQLD_READER)
+                || pluginName.equalsIgnoreCase(PluginNameConstrant.POSTGRESQL_READER)
+                || pluginName.equalsIgnoreCase(PluginNameConstrant.MYSQL_WRITER)
+                || pluginName.equalsIgnoreCase(PluginNameConstrant.GBASE_WRITER) ){
             String[] splits = dbUrl.split("\\?");
 
             Map<String,String> paramMap = new HashMap<String,String>();
@@ -609,9 +612,9 @@ public class DBUtil {
             }
 
             paramMap.put("useCursorFetch", "true");
-
-            if(pluginName.equalsIgnoreCase("mysqlreader")
-                    || pluginName.equalsIgnoreCase("mysqldreader")){
+            paramMap.put("rewriteBatchedStatements", "true");
+            if(pluginName.equalsIgnoreCase(PluginNameConstrant.MYSQL_READER)
+                    || pluginName.equalsIgnoreCase(PluginNameConstrant.MYSQLD_READER)){
                 paramMap.put("zeroDateTimeBehavior","convertToNull");
             }
 
