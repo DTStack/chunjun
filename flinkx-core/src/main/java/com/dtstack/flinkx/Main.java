498fd0186974d162acf93b0d2b092a0e286f06eb
jiangbo <jiangbo@dtstack.com>
2019-07-22 17:52:48 +0800

    add mode param

--- flinkx-core/src/main/java/com/dtstack/flinkx/Main.java
+++ flinkx-core/src/main/java/com/dtstack/flinkx/Main.java
@@ -51,13 +51,16 @@ public class Main {
 
         // 解析命令行参数
         Options options = new Options();
+        options.addOption("mode", true, "Job run mode");
         options.addOption("job", true, "Job config.");
         options.addOption("jobid", true, "Job unique id.");
         options.addOption("monitor", true, "Monitor Addresses");
         options.addOption("pluginRoot", true, "plugin path root");
+
         BasicParser parser = new BasicParser();
         CommandLine cl = parser.parse(options, args);
         String job = cl.getOptionValue("job");
+        String mode=cl.getOptionValue("mode");
         String jobIdString = cl.getOptionValue("jobid");
         String monitor = cl.getOptionValue("monitor");
         String pluginRoot = cl.getOptionValue("pluginRoot");
@@ -74,7 +77,6 @@ public class Main {
         if(StringUtils.isNotEmpty(pluginRoot)) {
             config.setPluginRoot(pluginRoot);
         }
-
 
         //构造并执行flink任务
         StreamExecutionEnvironment env = (StringUtils.isNotBlank(monitor)) ?
