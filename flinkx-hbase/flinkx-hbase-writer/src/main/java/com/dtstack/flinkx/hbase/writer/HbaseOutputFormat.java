d28f79ecd2760e137b2308c60d14329050917ac9
sishu@dtstack.com <sishu@dtstack.com>
2019-08-05 19:34:54 +0800

    performance

--- flinkx-hbase/flinkx-hbase-writer/src/main/java/com/dtstack/flinkx/hbase/writer/HbaseOutputFormat.java
+++ flinkx-hbase/flinkx-hbase-writer/src/main/java/com/dtstack/flinkx/hbase/writer/HbaseOutputFormat.java
@@ -37,6 +37,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import org.apache.flink.hadoop.shaded.com.google.common.collect.Maps;
 
 /**
  * The Hbase Implementation of OutputFormat
@@ -77,6 +78,9 @@ public class HbaseOutputFormat extends RichOutputFormat {
     private transient Connection connection;
 
     private transient BufferedMutator bufferedMutator;
+
+    private transient Map<String,String[]> nameMaps = Maps.newConcurrentMap();
+    private transient Map<String, byte[][]> nameByteMaps = Maps.newConcurrentMap();
 
     @Override
     public void configure(Configuration parameters) {
@@ -128,19 +132,26 @@ public class HbaseOutputFormat extends RichOutputFormat {
                 String type = columnTypes.get(i);
                 ColumnType columnType = ColumnType.getByTypeName(type);
                 String name =columnNames.get(i);
-                String promptInfo = "Hbasewriter 中，column 的列配置格式应该是：列族:列名. 您配置的列错误：" + name;
-                String[] cfAndQualifier = name.split(":");
-
-                Validate.isTrue(cfAndQualifier != null && cfAndQualifier.length == 2
-                        && org.apache.commons.lang3.StringUtils.isNotBlank(cfAndQualifier[0])
-                        && org.apache.commons.lang3.StringUtils.isNotBlank(cfAndQualifier[1]), promptInfo);
-
+                String[] cfAndQualifier = nameMaps.get(name);
+                byte[][] cfAndQualifierBytes = nameByteMaps.get(name);
+                if(cfAndQualifier == null || cfAndQualifierBytes==null){
+                    String promptInfo = "Hbasewriter 中，column 的列配置格式应该是：列族:列名. 您配置的列错误：" + name;
+                    cfAndQualifier = name.split(":");
+                    Validate.isTrue(cfAndQualifier != null && cfAndQualifier.length == 2
+                            && org.apache.commons.lang3.StringUtils.isNotBlank(cfAndQualifier[0])
+                            && org.apache.commons.lang3.StringUtils.isNotBlank(cfAndQualifier[1]), promptInfo);
+                    nameMaps.put(name,cfAndQualifier);
+                    cfAndQualifierBytes = new byte[2][];
+                    cfAndQualifierBytes[0] = Bytes.toBytes(cfAndQualifier[0]);
+                    cfAndQualifierBytes[1] = Bytes.toBytes(cfAndQualifier[1]);
+                    nameByteMaps.put(name,cfAndQualifierBytes);
+                }
                 byte[] columnBytes = getColumnByte(columnType,record.getField(i));
                 //columnBytes 为null忽略这列
                 if(null != columnBytes){
-                    put.addColumn(Bytes.toBytes(
-                            cfAndQualifier[0]),
-                            Bytes.toBytes(cfAndQualifier[1]),
+                    put.addColumn(
+                            cfAndQualifierBytes[0],
+                            cfAndQualifierBytes[1],
                             columnBytes);
                 }else{
                     continue;
