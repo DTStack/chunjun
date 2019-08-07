4471161ac15f576a5e5a231bf81a5ce0f67b8b5d
953372946@qq.com <953372946@qq.com>
2019-08-07 17:40:33 +0800

    init

---
+++ flinkx-hive/flinkx-hive-core/src/main/java/com/dtstack/flinkx/hive/HdfsUtil.java
@@ -0,0 +1,229 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.flinkx.hive;
+
+import com.dtstack.flinkx.enums.ColumnType;
+import com.dtstack.flinkx.util.DateUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
+import org.apache.hadoop.hive.serde2.io.DateWritable;
+import org.apache.hadoop.hive.serde2.io.HiveDecimalWritable;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.io.*;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+/**
+ * Utilities for HdfsReader and HdfsWriter
+ *
+ * Company: www.dtstack.com
+ * @author huyifan.zju@163.com
+ */
+public class HdfsUtil {
+
+    private static final String HADOOP_CONFIGE = System.getProperty("user.dir") + "/conf/hadoop/";
+
+    private static final String HADOOP_CONF_DIR = System.getenv("HADOOP_CONF_DIR");
+
+    private static Configuration configuration = new Configuration();
+
+    static {
+        try {
+            String dir = StringUtils.isNotBlank(HADOOP_CONF_DIR)?HADOOP_CONF_DIR:HADOOP_CONFIGE;
+            configuration.set("fs.hdfs.impl", DistributedFileSystem.class.getName());
+            configuration.set("fs.hdfs.impl.disable.cache", "true");
+            File[] xmlFileList = new File(dir).listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    if(name.endsWith(".xml")){
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+            if(xmlFileList != null) {
+                for(File xmlFile : xmlFileList) {
+                    configuration.addResource(xmlFile.toURI().toURL());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static Configuration getConfiguration(){
+        return configuration;
+    }
+
+    public static String getDefaultFs(){
+        return configuration.get("fs.defaultFS");
+    }
+
+    public static Configuration getHadoopConfig(Map<String,String> confMap, String defaultFS){
+        Configuration conf = new Configuration();
+
+        if (confMap != null){
+            for (Map.Entry<String, String> entry : confMap.entrySet()) {
+                conf.set(entry.getKey(), entry.getValue());
+            }
+        }
+
+        conf.set("fs.default.name", defaultFS);
+        conf.set("fs.hdfs.impl.disable.cache", "true");
+
+        return conf;
+    }
+
+    public static Object string2col(String str, String type, SimpleDateFormat customDateFormat) {
+        if (str == null || str.length() == 0){
+            return null;
+        }
+
+        if(type == null){
+            return str;
+        }
+
+        ColumnType columnType = ColumnType.fromString(type.toUpperCase());
+        Object ret;
+        switch(columnType) {
+            case TINYINT:
+                ret = Byte.valueOf(str.trim());
+                break;
+            case SMALLINT:
+                ret = Short.valueOf(str.trim());
+                break;
+            case INT:
+                ret = Integer.valueOf(str.trim());
+                break;
+            case BIGINT:
+                ret = Long.valueOf(str.trim());
+                break;
+            case FLOAT:
+                ret = Float.valueOf(str.trim());
+                break;
+            case DOUBLE:
+            case DECIMAL:
+                ret = Double.valueOf(str.trim());
+                break;
+            case STRING:
+            case VARCHAR:
+            case CHAR:
+                if(customDateFormat != null){
+                    ret = DateUtil.columnToDate(str,customDateFormat);
+                    ret = DateUtil.timestampToString((Date)ret);
+                } else {
+                    ret = str;
+                }
+                break;
+            case BOOLEAN:
+                ret = Boolean.valueOf(str.trim().toLowerCase());
+                break;
+            case DATE:
+                ret = DateUtil.columnToDate(str,customDateFormat);
+                break;
+            case TIMESTAMP:
+                ret = DateUtil.columnToTimestamp(str,customDateFormat);
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported field type:" + type);
+        }
+
+        return ret;
+    }
+
+    public static Object getWritableValue(Object writable) {
+        Class<?> clz = writable.getClass();
+        Object ret = null;
+
+        if(clz == IntWritable.class) {
+            ret = ((IntWritable) writable).get();
+        } else if(clz == Text.class) {
+            ret = ((Text) writable).toString();
+        } else if(clz == LongWritable.class) {
+            ret = ((LongWritable) writable).get();
+        } else if(clz == ByteWritable.class) {
+            ret = ((ByteWritable) writable).get();
+        } else if(clz == DateWritable.class) {
+            ret = ((DateWritable) writable).get();
+        } else if(writable instanceof DoubleWritable){
+            ret = ((DoubleWritable) writable).get();
+        } else if(writable instanceof Writable) {
+            ret = writable.toString();
+        } else {
+            ret = writable.toString();
+        }
+
+        return ret;
+    }
+
+    public static ObjectInspector columnTypeToObjectInspetor(ColumnType columnType) {
+        ObjectInspector objectInspector = null;
+        switch(columnType) {
+            case TINYINT:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(Byte.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case SMALLINT:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(Short.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case INT:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(Integer.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case BIGINT:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(Long.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case FLOAT:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(Float.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case DOUBLE:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(Double.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case DECIMAL:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(HiveDecimalWritable.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case TIMESTAMP:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(java.sql.Timestamp.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case DATE:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(java.sql.Date.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case STRING:
+            case VARCHAR:
+            case CHAR:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(String.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case BOOLEAN:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(Boolean.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            case BINARY:
+                objectInspector = ObjectInspectorFactory.getReflectionObjectInspector(BytesWritable.class, ObjectInspectorFactory.ObjectInspectorOptions.JAVA);
+                break;
+            default:
+                throw new IllegalArgumentException("You should not be here");
+        }
+        return objectInspector;
+    }
+
+}
---
+++ flinkx-hive/flinkx-hive-writer/src/main/java/com/dtstack/flinkx/hive/writer/HiveWriter.java
@@ -0,0 +1,161 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dtstack.flinkx.hive.writer;
+
+import com.dtstack.flinkx.config.DataTransferConfig;
+import com.dtstack.flinkx.config.WriterConfig;
+import com.dtstack.flinkx.writer.DataWriter;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.functions.sink.DtOutputFormatSinkFunction;
+import org.apache.flink.types.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import parquet.hadoop.ParquetWriter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import static com.dtstack.flinkx.hive.HdfsConfigKeys.*;
+
+/**
+ * The writer plugin of Hdfs
+ *
+ * Company: www.dtstack.com
+ * @author huyifan.zju@163.com
+ */
+public class HiveWriter extends DataWriter {
+
+    protected final Logger LOG = LoggerFactory.getLogger(getClass());
+
+    protected String defaultFS;
+
+    protected String fileType;
+
+    protected String path;
+
+    protected String fieldDelimiter;
+
+    protected String compress;
+
+    protected String fileName;
+
+    protected String tablesColumn;
+
+    protected List<String> columnName;
+
+    protected List<String> columnType;
+
+    protected Map<String,String> hadoopConfig;
+
+    protected String charSet;
+
+    protected List<String> fullColumnName;
+
+    protected List<String> fullColumnType;
+
+    protected static final String DATA_SUBDIR = ".data";
+
+    protected static final String FINISHED_SUBDIR = ".finished";
+
+    protected static final String SP = "/";
+
+    protected int rowGroupSize;
+
+    protected long maxFileSize;
+
+
+    private String jdbcUrl;
+
+    private String database;
+
+    private String username;
+
+    private String password;
+
+    public HiveWriter(DataTransferConfig config) {
+        super(config);
+        WriterConfig writerConfig = config.getJob().getContent().get(0).getWriter();
+        hadoopConfig = (Map<String, String>) writerConfig.getParameter().getVal(KEY_HADOOP_CONFIG_MAP);
+        tablesColumn = writerConfig.getParameter().getStringVal(KEY_TABLE_COLUMN);
+        fileType = writerConfig.getParameter().getStringVal(KEY_STORE);
+        defaultFS = writerConfig.getParameter().getStringVal(KEY_DEFAULT_FS);
+        path = writerConfig.getParameter().getStringVal(KEY_PATH);
+        fieldDelimiter = writerConfig.getParameter().getStringVal(KEY_FIELD_DELIMITER);
+        charSet = writerConfig.getParameter().getStringVal(KEY_ENCODING);
+        rowGroupSize = writerConfig.getParameter().getIntVal(KEY_ROW_GROUP_SIZE, ParquetWriter.DEFAULT_BLOCK_SIZE);
+        maxFileSize = writerConfig.getParameter().getLongVal(KEY_MAX_FILE_SIZE, 1024 * 1024 * 1024);
+
+        if(fieldDelimiter == null || fieldDelimiter.length() == 0) {
+            fieldDelimiter = "\001";
+        } else {
+            fieldDelimiter = com.dtstack.flinkx.util.StringUtil.convertRegularExpr(fieldDelimiter);
+        }
+
+        compress = writerConfig.getParameter().getStringVal(KEY_COMPRESS);
+        fileName = writerConfig.getParameter().getStringVal(KEY_FILE_NAME, "");
+        if(CollectionUtils.isNotEmpty(columns)) {
+            columnName = new ArrayList<>();
+            columnType = new ArrayList<>();
+            for (Object column : columns) {
+                Map sm = (Map) column;
+                columnName.add((String) sm.get(KEY_COLUMN_NAME));
+                columnType.add((String) sm.get(KEY_COLUMN_TYPE));
+            }
+        }
+
+        fullColumnName = (List<String>) writerConfig.getParameter().getVal(KEY_FULL_COLUMN_NAME_LIST);
+        fullColumnType = (List<String>) writerConfig.getParameter().getVal(KEY_FULL_COLUMN_TYPE_LIST);
+
+        mode = writerConfig.getParameter().getStringVal(KEY_WRITE_MODE);
+    }
+
+    @Override
+    public DataStreamSink<?> writeData(DataStream<Row> dataSet) {
+        HdfsOutputFormatBuilder builder = new HdfsOutputFormatBuilder(fileType);
+        builder.setHadoopConfig(hadoopConfig);
+        builder.setDefaultFS(defaultFS);
+        builder.setPath(path);
+        builder.setFileName(fileName);
+        builder.setWriteMode(mode);
+        builder.setColumnNames(columnName);
+        builder.setColumnTypes(columnType);
+        builder.setCompress(compress);
+        builder.setMonitorUrls(monitorUrls);
+        builder.setErrors(errors);
+        builder.setErrorRatio(errorRatio);
+        builder.setFullColumnNames(fullColumnName);
+        builder.setFullColumnTypes(fullColumnType);
+        builder.setDirtyPath(dirtyPath);
+        builder.setDirtyHadoopConfig(dirtyHadoopConfig);
+        builder.setSrcCols(srcCols);
+        builder.setCharSetName(charSet);
+        builder.setDelimiter(fieldDelimiter);
+        builder.setRowGroupSize(rowGroupSize);
+        builder.setRestoreConfig(restoreConfig);
+        builder.setMaxFileSize(maxFileSize);
+
+        DtOutputFormatSinkFunction sinkFunction = new DtOutputFormatSinkFunction(builder.finish());
+        DataStreamSink<?> dataStreamSink = dataSet.addSink(sinkFunction);
+
+        dataStreamSink.name("hdfswriter");
+
+        return dataStreamSink;
+    }
+}
