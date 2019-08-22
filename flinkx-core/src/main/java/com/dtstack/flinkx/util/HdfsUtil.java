d9ca496368948d12c329fbd35a5f7caffa5eabe6
jiangbo <jiangbo@dtstack.com>
2019-08-22 16:43:48 +0800

    update

--- flinkx-core/src/main/java/com/dtstack/flinkx/util/HdfsUtil.java
+++ flinkx-core/src/main/java/com/dtstack/flinkx/util/HdfsUtil.java
@@ -21,9 +21,12 @@ package com.dtstack.flinkx.util;
 
 import com.dtstack.flinkx.authenticate.KerberosUtil;
 import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.security.UserGroupInformation;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -32,6 +35,7 @@ import java.util.Map;
  */
 public class HdfsUtil {
 
+    private static final String KEY_JAVA_SECURITY_KRB5_CONF = "java.security.krb5.conf";
     private static final String AUTHENTICATION_TYPE = "Kerberos";
     private static final String KEY_HADOOP_SECURITY_AUTHORIZATION = "hadoop.security.authorization";
     private static final String KEY_HADOOP_SECURITY_AUTHENTICATION = "hadoop.security.authentication";
@@ -53,10 +57,19 @@ public class HdfsUtil {
         return AUTHENTICATION_TYPE.equalsIgnoreCase(MapUtils.getString(hadoopConfig, KEY_HADOOP_SECURITY_AUTHENTICATION));
     }
 
-    private static void login(Map<String, Object> hadoopConfig, String jobId, String plugin){
+    private static void login(Map<String, Object> hadoopConfig, String jobId, String plugin) throws IOException {
         KerberosUtil.loadKeyTabFilesAndReplaceHost(hadoopConfig, jobId, plugin);
 
+        Configuration conf = getHadoopConfig(hadoopConfig, null);
+        if(StringUtils.isNotEmpty(conf.get(KEY_JAVA_SECURITY_KRB5_CONF))){
+            System.setProperty(KEY_JAVA_SECURITY_KRB5_CONF, conf.get(KEY_JAVA_SECURITY_KRB5_CONF));
+        }
 
+        UserGroupInformation.setConfiguration(conf);
+
+        String principal = "";
+        String keytab = "";
+        UserGroupInformation.loginUserFromKeytab(principal, keytab);
     }
 
     public static Configuration getHadoopConfig(Map<String, Object> confMap, String defaultFS) {
@@ -70,7 +83,10 @@ public class HdfsUtil {
             }
         }
 
-        conf.set("fs.default.name", defaultFS);
+        if(defaultFS != null){
+            conf.set("fs.default.name", defaultFS);
+        }
+
         conf.set("fs.hdfs.impl.disable.cache", "true");
 
         return conf;
