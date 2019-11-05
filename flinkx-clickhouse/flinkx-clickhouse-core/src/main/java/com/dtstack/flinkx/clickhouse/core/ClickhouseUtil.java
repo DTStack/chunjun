83709671cf7309f9077ec4a989858d7dde59f469
tudou <tudou@dtstack.com>
2019-11-05 22:08:22 +0800

    temp commit

---
+++ flinkx-clickhouse/flinkx-clickhouse-core/src/main/java/com/dtstack/flinkx/clickhouse/core/ClickhouseUtil.java
@@ -0,0 +1,114 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dtstack.flinkx.clickhouse.core;
+
+import com.dtstack.flinkx.util.ExceptionUtil;
+import com.dtstack.flinkx.util.SysUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.yandex.clickhouse.BalancedClickhouseDataSource;
+import ru.yandex.clickhouse.settings.ClickHouseProperties;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * Date: 2019/11/05
+ * Company: www.dtstack.com
+ *
+ * @author tudou
+ */
+public class ClickhouseUtil {
+    private static final Logger LOG = LoggerFactory.getLogger(ClickhouseUtil.class);
+
+    private static final int MAX_RETRY_TIMES = 3;
+
+    public static Connection getConnection(String url, String username, String password, Properties properties) throws SQLException {
+        ClickHouseProperties p = new ClickHouseProperties(properties);
+        p.withCredentials(username, password);
+        boolean failed = true;
+        Connection conn = null;
+        for (int i = 0; i < MAX_RETRY_TIMES && failed; ++i) {
+            try {
+                conn = new BalancedClickhouseDataSource(url, properties).getConnection();
+                conn.createStatement().execute("select 111");
+                failed = false;
+            } catch (Exception e) {
+                if (conn != null) {
+                    conn.close();
+                }
+                if (i == MAX_RETRY_TIMES - 1) {
+                    throw e;
+                } else {
+                    SysUtil.sleep(3000);
+                }
+            }
+        }
+
+        return conn;
+    }
+
+    public static void closeDBResources(ResultSet rs, Statement stmt, Connection conn) {
+        if (null != rs) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                LOG.warn("Close resultSet error: {}", ExceptionUtil.getErrorMessage(e));
+            }
+        }
+
+        if (null != stmt) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                LOG.warn("Close statement error:{}", ExceptionUtil.getErrorMessage(e));
+            }
+        }
+
+        if (null != conn) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                LOG.warn("Close connection error:{}", ExceptionUtil.getErrorMessage(e));
+            }
+        }
+    }
+
+    public static Object getValue(ResultSet rs, String name, String type) throws SQLException{
+        switch (type.toLowerCase()) {
+            case "IntervalYear":
+            case "IntervalQuarter":
+            case "IntervalMonth":
+            case "IntervalWeek":
+            case "IntervalDay":
+            case "IntervalHour":
+            case "IntervalMinute":
+            case "IntervalSecond":
+            case "UInt32":
+            case "UInt16":
+            case "UInt8":
+            case "Int32":   return rs.getInt(name);
+            case "UInt64":
+            case "Int64":
+            default:    return rs.getString(name);
+        }
+    }
+}
---
+++ flinkx-clickhouse/flinkx-clickhouse-writer/src/main/java/com/dtstack/flinkx/clickhouse/writer/ClickhouseWriter.java
@@ -0,0 +1,10 @@
+package com.dtstack.flinkx.clickhouse.writer;
+
+/**
+ * Date: 2019/11/05
+ * Company: www.dtstack.com
+ *
+ * @author tudou
+ */
+public class ClickhouseWriter {
+}
