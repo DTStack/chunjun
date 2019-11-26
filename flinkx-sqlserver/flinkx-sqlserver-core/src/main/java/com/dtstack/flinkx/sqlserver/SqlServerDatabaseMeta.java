63b4a324decfb024f2012d4db64ec50eae4ef4a5
tudou <tudou@dtstack.com>
2019-11-26 16:59:26 +0800

    修改更新列

--- flinkx-sqlserver/flinkx-sqlserver-core/src/main/java/com/dtstack/flinkx/sqlserver/SqlServerDatabaseMeta.java
+++ flinkx-sqlserver/flinkx-sqlserver-core/src/main/java/com/dtstack/flinkx/sqlserver/SqlServerDatabaseMeta.java
@@ -100,7 +100,7 @@ public class SqlServerDatabaseMeta extends BaseDatabaseMeta {
             return "MERGE INTO " + quoteTable(table) + " T1 USING "
                     + "(" + makeValues(column) + ") T2 ON ("
                     + updateKeySql(updateKey) + ") WHEN MATCHED THEN UPDATE SET "
-                    + getSqlServerUpdateSql(column, updateKey,"T1", "T2") + " WHEN NOT MATCHED THEN "
+                    + getSqlServerUpdateSql(updateColumns, updateKey,"T1", "T2") + " WHEN NOT MATCHED THEN "
                     + "INSERT (" + quoteColumns(column) + ") VALUES ("
                     + quoteColumns(column, "T2") + ");";
         }
