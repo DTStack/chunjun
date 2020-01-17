a9fdb052d6a8452ad49b5a49321770e4f53205dc
jiangbo <jiangbo@dtstack.com>
2020-01-17 16:47:30 +0800

    打包前install部分驱动包

---
+++ bin/install_jars.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+## db2 driver
+mvn install:install-file -DgroupId=com.ibm.db2 -DartifactId=db2jcc -Dversion=3.72.44 -Dpackaging=jar -Dfile=../jars/db2jcc-3.72.44.jar
+
+## oracle driver
+mvn install:install-file -DgroupId=com.github.noraui -DartifactId=ojdbc8 -Dversion=12.2.0.1 -Dpackaging=jar -Dfile=../jars/ojdbc8-12.2.0.1.jar
+
+## gbase driver
+mvn install:install-file -DgroupId=com.esen.jdbc -DartifactId=gbase -Dversion=8.3.81.53 -Dpackaging=jar -Dfile=../jars/gbase-8.3.81.53.jar
+
