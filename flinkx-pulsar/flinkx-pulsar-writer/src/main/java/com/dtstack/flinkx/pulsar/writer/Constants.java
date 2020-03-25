b002162cb8364929e8b04d91e8b330aac0df57f7
pierrexiong <pierrexiong@tencent.com>
2020-03-25 13:48:56 +0800

    A simple pulsar writer

---
+++ flinkx-pulsar/flinkx-pulsar-writer/src/main/java/com/dtstack/flinkx/pulsar/writer/Constants.java
@@ -0,0 +1,13 @@
+package com.dtstack.flinkx.pulsar.writer;
+
+/**
+ * @author: pierre
+ * @create: 2020/3/21
+ */
+public class Constants {
+    public static final String KEY_TOPIC = "topic";
+    public static final String KEY_PULSAR_SERVICE_URL = "pulsarServiceUrl";
+    public static final String KEY_PRODUCER_SETTINGS = "producerSettings";
+    public static final String KEY_TABLE_FIELDS = "tableFields";
+    public static final String KEY_TOKEN = "token";
+}
