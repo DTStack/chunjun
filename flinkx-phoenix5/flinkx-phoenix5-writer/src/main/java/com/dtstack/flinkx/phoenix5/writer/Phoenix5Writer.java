2545d90369bc4fad46b5bf60976d26ecd8d42011
lvyanquan <kunni@dtstack.com>
2020-06-29 10:56:58 +0800

    phoenix5 framework

---
+++ flinkx-phoenix5/flinkx-phoenix5-writer/src/main/java/com/dtstack/flinkx/phoenix5/writer/Phoenix5Writer.java
@@ -0,0 +1,34 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.flinkx.phoenix5.writer;
+
+import com.dtstack.flinkx.config.DataTransferConfig;
+import com.dtstack.flinkx.phoenix.writer.PhoenixWriter;
+
+/**
+ * Company: www.dtstack.com
+ *
+ * @author kunni
+ */
+
+public class Phoenix5Writer extends PhoenixWriter {
+    public Phoenix5Writer(DataTransferConfig config) {
+        super(config);
+    }
+}
