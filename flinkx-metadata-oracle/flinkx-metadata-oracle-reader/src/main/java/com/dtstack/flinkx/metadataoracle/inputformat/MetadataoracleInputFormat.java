0e6232e9fdb87ba2aa290b0942a3abeb421fb283
lvyanquan <kunni@dtstack.com>
2020-08-22 16:31:33 +0800

    fix: 一次查询多张表信息

--- flinkx-metadata-oracle/flinkx-metadata-oracle-reader/src/main/java/com/dtstack/flinkx/metadataoracle/inputformat/MetadataoracleInputFormat.java
+++ flinkx-metadata-oracle/flinkx-metadata-oracle-reader/src/main/java/com/dtstack/flinkx/metadataoracle/inputformat/MetadataoracleInputFormat.java
@@ -18,6 +18,7 @@
 
 package com.dtstack.flinkx.metadataoracle.inputformat;
 
+import com.dtstack.flinkx.constants.ConstantValue;
 import com.dtstack.flinkx.metadata.inputformat.BaseMetadataInputFormat;
 import org.apache.commons.collections.CollectionUtils;
 
@@ -52,6 +53,14 @@ import static com.dtstack.flinkx.metadataoracle.constants.OracleMetaDataCons.SQL
 
 public class MetadataoracleInputFormat extends BaseMetadataInputFormat {
 
+    private Map<String, Map<String, String>> tablePropertiesMap;
+
+    private Map<String, List<Map<String, Object>>> columnListMap;
+
+    private Map<String, List<Map<String, String>>> indexListMap;
+
+    private String allTable;
+
     @Override
     protected List<String> showTables() throws SQLException {
         List<String> tableNameList = new LinkedList<>();
@@ -77,59 +86,90 @@ public class MetadataoracleInputFormat extends BaseMetadataInputFormat {
     @Override
     protected Map<String, Object> queryMetaData(String tableName) throws SQLException {
         Map<String, Object> result = new HashMap<>(16);
-        Map<String, String> tableProperties = queryTableProperties(tableName);
-        List<Map<String, Object>> columnList = queryColumnList(tableName);
-        List<Map<String, String>> indexList = queryIndexList(tableName);
+        Map<String, String> tableProperties = tablePropertiesMap.get(tableName);
+        List<Map<String, Object>> columnList = columnListMap.get(tableName);
+        List<Map<String, String>> indexList = indexListMap.get(tableName);
         result.put(KEY_TABLE_PROPERTIES, tableProperties);
         result.put(KEY_COLUMN, columnList);
         result.put(KEY_COLUMN_INDEX, indexList);
         return result;
     }
 
-    Map<String, String> queryTableProperties(String tableName) throws SQLException {
-        Map<String, String> tableProperties = new HashMap<>(16);
-        String sql = String.format(SQL_QUERY_TABLE_PROPERTIES, quote(currentDb.get()), quote(tableName));
+    Map<String, Map<String, String> > queryTableProperties() throws SQLException {
+        Map<String, Map<String, String>> tablePropertiesMap = new HashMap<>(16);
+        String sql = String.format(SQL_QUERY_TABLE_PROPERTIES, quote(currentDb.get()), allTable);
         try (ResultSet rs = statement.get().executeQuery(sql)) {
             while (rs.next()) {
-                tableProperties.put(KEY_TOTAL_SIZE, rs.getString(1));
-                tableProperties.put(KEY_COLUMN_COMMENT, rs.getString(2));
-                tableProperties.put(KEY_TABLE_TYPE, rs.getString(3));
-                tableProperties.put(KEY_CREATE_TIME, rs.getString(4));
-                tableProperties.put(KEY_ROWS, rs.getString(5));
+                Map<String, String> map = new HashMap<>(16);
+                map.put(KEY_TOTAL_SIZE, rs.getString(1));
+                map.put(KEY_COLUMN_COMMENT, rs.getString(2));
+                map.put(KEY_TABLE_TYPE, rs.getString(3));
+                map.put(KEY_CREATE_TIME, rs.getString(4));
+                map.put(KEY_ROWS, rs.getString(5));
+                tablePropertiesMap.put(rs.getString(6), map);
             }
         }
-        return tableProperties;
+        return tablePropertiesMap;
     }
 
-    List<Map<String, String>> queryIndexList(String tableName) throws SQLException {
-        List<Map<String, String>>indexList  = new LinkedList<>();
-        String sql = String.format(SQL_QUERY_INDEX, quote(currentDb.get()), quote(tableName));
+    Map<String, List<Map<String, String>>> queryIndexList() throws SQLException {
+        Map<String, List<Map<String, String>>> indexListMap = new HashMap<>(16);
+        String sql = String.format(SQL_QUERY_INDEX, quote(currentDb.get()), allTable);
         try (ResultSet rs = statement.get().executeQuery(sql)) {
             while (rs.next()) {
                 Map<String, String> column = new HashMap<>(16);
                 column.put(KEY_COLUMN_NAME, rs.getString(1));
                 column.put(KEY_INDEX_COLUMN_NAME, rs.getString(2));
                 column.put(KEY_COLUMN_TYPE, rs.getString(3));
-                indexList.add(column);
+                String tableName = rs.getString(4);
+                if(indexListMap.containsKey(tableName)){
+                    indexListMap.get(tableName).add(column);
+                }else {
+                    List<Map<String, String>> indexList  = new LinkedList<>();
+                    indexList.add(column);
+                    indexListMap.put(tableName, indexList);
+                }
             }
         }
-        return indexList;
+        return indexListMap;
     }
 
-    List<Map<String, Object>> queryColumnList(String tableName) throws SQLException {
-        List<Map<String, Object>>columnList  = new LinkedList<>();
-        String sql = String.format(SQL_QUERY_COLUMN, quote(currentDb.get()), quote(tableName));
+    Map<String, List<Map<String, Object>>> queryColumnList() throws SQLException {
+        Map<String, List<Map<String, Object>>> columnListMap = new HashMap<>(16);
+        String sql = String.format(SQL_QUERY_COLUMN, quote(currentDb.get()), allTable);
         try (ResultSet rs = statement.get().executeQuery(sql)) {
             while (rs.next()) {
                 Map<String, Object> column = new HashMap<>(16);
                 column.put(KEY_COLUMN_NAME, rs.getString(1));
                 column.put(KEY_COLUMN_TYPE, rs.getString(2));
                 column.put(KEY_COLUMN_COMMENT, rs.getString(3));
-                column.put(KEY_COLUMN_INDEX, CollectionUtils.size(columnList)+1);
-                columnList.add(column);
+                String tableName = rs.getString(4);
+                if(columnListMap.containsKey(tableName)){
+                    column.put(KEY_COLUMN_INDEX, CollectionUtils.size(columnListMap.get(tableName))+1);
+                    columnListMap.get(tableName).add(column);
+                }else {
+                    List<Map<String, Object>>columnList  = new LinkedList<>();
+                    column.put(KEY_COLUMN_INDEX, 1);
+                    columnList.add(column);
+                    columnListMap.put(tableName, columnList);
+                }
             }
         }
-        return columnList;
+        return columnListMap;
     }
 
+    @Override
+    protected void init() throws SQLException {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int index=0;index<tableList.size();index++){
+            stringBuilder.append(quote(tableList.get(index)));
+            if(index!=tableList.size()-1){
+                stringBuilder.append(ConstantValue.COMMA_SYMBOL);
+            }
+        }
+        allTable = stringBuilder.toString();
+        tablePropertiesMap = queryTableProperties();
+        columnListMap = queryColumnList();
+        indexListMap = queryIndexList();
+    }
 }
--- flinkx-metadata/flinkx-metadata-reader/src/main/java/com/dtstack/flinkx/metadata/inputformat/BaseMetadataInputFormat.java
+++ flinkx-metadata/flinkx-metadata-reader/src/main/java/com/dtstack/flinkx/metadata/inputformat/BaseMetadataInputFormat.java
@@ -52,6 +52,8 @@ public abstract class BaseMetadataInputFormat extends BaseRichInputFormat {
 
     protected List<Map<String, Object>> dbTableList;
 
+    protected List<String> tableList;
+
     protected static transient ThreadLocal<Connection> connection = new ThreadLocal<>();
 
     protected static transient ThreadLocal<Statement> statement = new ThreadLocal<>();
@@ -68,11 +70,12 @@ public abstract class BaseMetadataInputFormat extends BaseRichInputFormat {
             statement.set(connection.get().createStatement());
             currentDb.set(((MetadataInputSplit) inputSplit).getDbName());
             switchDatabase(currentDb.get());
-            List<String> tableList = ((MetadataInputSplit) inputSplit).getTableList();
+            tableList = ((MetadataInputSplit) inputSplit).getTableList();
             if (CollectionUtils.isEmpty(tableList)) {
                 tableList = showTables();
             }
             tableIterator.set(tableList.iterator());
+            init();
         } catch (SQLException | ClassNotFoundException e) {
             LOG.error("获取table列表异常, dbUrl = {}, username = {}, inputSplit = {}, e = {}", dbUrl, username, inputSplit, ExceptionUtil.getErrorMessage(e));
             throw new IOException("获取table列表异常", e);
@@ -194,4 +197,7 @@ public abstract class BaseMetadataInputFormat extends BaseRichInputFormat {
      * @return 返回数据库名，表名，列名的引用
      */
     protected abstract String quote(String name);
+
+    protected void init() throws SQLException {}
+
 }
