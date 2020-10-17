e7e348ba2ea537014ffc838748289fb683eb6394
lvyanquan <kunni@dtstack.com>
2020-10-17 14:07:43 +0800

    hotfix:【在发送端增加幂等标识】【31374】

--- flinkx-restapi/flinkx-restapi-writer/src/main/java/com/dtstack/flinkx/restapi/outputformat/RestapiOutputFormat.java
+++ flinkx-restapi/flinkx-restapi-writer/src/main/java/com/dtstack/flinkx/restapi/outputformat/RestapiOutputFormat.java
@@ -37,6 +37,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.dtstack.flinkx.restapi.common.RestapiKeys.KEY_BATCH;
 
 /**
  * @author : tiezhu
@@ -57,9 +60,11 @@ public class RestapiOutputFormat extends BaseRichOutputFormat {
 
     protected Map<String, String> header;
 
-    protected static final int DEFAULT_TIME_OUT = 300000;
+    protected static final int DEFAULT_TIME_OUT = 1800000;
 
     protected Gson gson;
+
+    protected AtomicInteger batchID = new AtomicInteger(0);
 
     @Override
     protected void openInternal(int taskNumber, int numTasks) throws IOException {
@@ -76,6 +81,7 @@ public class RestapiOutputFormat extends BaseRichOutputFormat {
         Object dataRow;
         try {
             dataRow = getDataFromRow(row, column);
+            params.put(KEY_BATCH, batchID.incrementAndGet());
             if (!params.isEmpty()) {
                 Iterator iterator = params.entrySet().iterator();
                 while (iterator.hasNext()) {
@@ -107,6 +113,7 @@ public class RestapiOutputFormat extends BaseRichOutputFormat {
             for (Row row : rows) {
                 dataRow.add(getDataFromRow(row, column));
             }
+            params.put(KEY_BATCH, batchID.incrementAndGet());
             if (!params.isEmpty()) {
                 Iterator iterator = params.entrySet().iterator();
                 while (iterator.hasNext()) {
