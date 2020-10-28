9f3bd4147ce297bfc55238506c89a61eadc04bc6
yanghuaiGit <2774584057@qq.com>
2020-10-28 19:31:56 +0800

    bug修复 优化kerberos支持

--- flinkx-core/src/main/java/com/dtstack/flinkx/util/GsonUtil.java
+++ flinkx-core/src/main/java/com/dtstack/flinkx/util/GsonUtil.java
@@ -35,6 +35,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
