f38005ffdb89020d8da1a2f00da75125e33cc351
lvyanquan <kunni@dtstack.com>
2020-11-03 14:25:14 +0800

    hoefix: 增加注释信息

--- flinkx-metadata-oracle/flinkx-metadata-oracle-reader/src/main/java/com/dtstack/flinkx/metadataoracle/constants/OracleMetaDataCons.java
+++ flinkx-metadata-oracle/flinkx-metadata-oracle-reader/src/main/java/com/dtstack/flinkx/metadataoracle/constants/OracleMetaDataCons.java
@@ -32,6 +32,9 @@ public class OracleMetaDataCons extends MetaDataCons {
 
     public static final String KEY_TABLE_TYPE = "tableType";
 
+    /**
+     * 通过in语法，减少内存占用
+     */
     public static final String SQL_QUERY_INDEX = "AND COLUMNS.TABLE_NAME IN (%s) ";
     public static final String SQL_QUERY_COLUMN = "AND COLUMNS.TABLE_NAME IN (%s) ";
     public static final String SQL_QUERY_PRIMARY_KEY = "AND COLUMNS.TABLE_NAME IN (%s) ";
@@ -39,17 +42,21 @@ public class OracleMetaDataCons extends MetaDataCons {
     public static final String SQL_QUERY_TABLE_CREATE_TIME = "AND TABLES.TABLE_NAME IN (%s) ";
 
     /**
-     * 使用ALL_系统表，降低权限要求
+     * 查询索引信息
      */
-
-    public static final String SQL_QUERY_INDEX_TOTAL = "SELECT COLUMNS.INDEX_NAME, COLUMNS.COLUMN_NAME, INDEXES.INDEX_TYPE, COLUMNS.TABLE_NAME  " +
+    public static final String SQL_QUERY_INDEX_TOTAL =
+            "SELECT COLUMNS.INDEX_NAME, COLUMNS.COLUMN_NAME, INDEXES.INDEX_TYPE, COLUMNS.TABLE_NAME  " +
             "FROM ALL_IND_COLUMNS COLUMNS  " +
             "LEFT JOIN ALL_INDEXES INDEXES  " +
             "ON COLUMNS.INDEX_NAME = INDEXES.INDEX_NAME  " +
             "AND COLUMNS.TABLE_NAME = INDEXES.TABLE_NAME  " +
             "AND COLUMNS.TABLE_OWNER = INDEXES.TABLE_OWNER  " +
             "WHERE COLUMNS.TABLE_OWNER = %s ";
-    public static final String SQL_QUERY_COLUMN_TOTAL = "SELECT COLUMNS.COLUMN_NAME, COLUMNS.DATA_TYPE, COMMENTS.COMMENTS, COLUMNS.TABLE_NAME, " +
+    /**
+     * 查询列的基本信息
+     */
+    public static final String SQL_QUERY_COLUMN_TOTAL =
+            "SELECT COLUMNS.COLUMN_NAME, COLUMNS.DATA_TYPE, COMMENTS.COMMENTS, COLUMNS.TABLE_NAME, " +
             "DATA_DEFAULT, NULLABLE, DATA_LENGTH  " +
             "FROM ALL_TAB_COLUMNS COLUMNS  " +
             "LEFT JOIN ALL_COL_COMMENTS COMMENTS  " +
@@ -57,19 +64,31 @@ public class OracleMetaDataCons extends MetaDataCons {
             "AND COLUMNS.TABLE_NAME = COMMENTS.TABLE_NAME  " +
             "AND COLUMNS.COLUMN_NAME = COMMENTS.COLUMN_NAME  " +
             "WHERE COLUMNS.OWNER = %s ";
-    public static final String SQL_QUERY_TABLE_PROPERTIES_TOTAL = "SELECT TABLES.NUM_ROWS * TABLES.AVG_ROW_LEN AS TOTALSIZE, " +
+    /**
+     * 查询表的基本信息
+     */
+    public static final String SQL_QUERY_TABLE_PROPERTIES_TOTAL =
+            "SELECT TABLES.NUM_ROWS * TABLES.AVG_ROW_LEN AS TOTALSIZE, " +
             "COMMENTS.COMMENTS, COMMENTS.TABLE_TYPE, TABLES.NUM_ROWS, TABLES.TABLE_NAME " +
             "FROM ALL_TABLES TABLES " +
             "LEFT JOIN ALL_TAB_COMMENTS COMMENTS " +
             "ON TABLES.OWNER = COMMENTS.OWNER " +
             "AND TABLES.TABLE_NAME = COMMENTS.TABLE_NAME " +
             "WHERE TABLES.OWNER = %s ";
-    public static final String SQL_QUERY_PRIMARY_KEY_TOTAL = "SELECT COLUMNS.TABLE_NAME, COLUMNS.COLUMN_NAME  " +
+    /**
+     * 查询主键信息
+     */
+    public static final String SQL_QUERY_PRIMARY_KEY_TOTAL =
+            "SELECT COLUMNS.TABLE_NAME, COLUMNS.COLUMN_NAME  " +
             "FROM ALL_CONS_COLUMNS COLUMNS, ALL_CONSTRAINTS CONS  " +
             "WHERE COLUMNS.CONSTRAINT_NAME = CONS.CONSTRAINT_NAME  " +
             "AND CONS.CONSTRAINT_TYPE = 'P'  " +
             "AND COLUMNS.OWNER = %s ";
-    public static final String SQL_QUERY_TABLE_CREATE_TIME_TOTAL = "SELECT TABLES.TABLE_NAME, OBJS.CREATED " +
+    /**
+     * 查询创建时间信息
+     */
+    public static final String SQL_QUERY_TABLE_CREATE_TIME_TOTAL =
+            "SELECT TABLES.TABLE_NAME, OBJS.CREATED " +
             "FROM ALL_TABLES TABLES " +
             "LEFT JOIN ALL_OBJECTS OBJS " +
             "ON TABLES.OWNER = OBJS.OWNER " +
@@ -80,5 +99,7 @@ public class OracleMetaDataCons extends MetaDataCons {
      * 查询特定schema下的所有表
      * 排除嵌套表等特殊表
      */
-    public static final String SQL_SHOW_TABLES = "SELECT TABLE_NAME FROM ALL_TABLES WHERE OWNER = %s AND NESTED = 'NO' AND IOT_NAME IS NULL ";
+    public static final String SQL_SHOW_TABLES =
+            "SELECT TABLE_NAME FROM ALL_TABLES " +
+            "WHERE OWNER = %s AND NESTED = 'NO' AND IOT_NAME IS NULL ";
 }
