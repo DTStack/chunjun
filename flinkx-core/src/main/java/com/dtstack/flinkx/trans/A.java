263204699387faee38f579862efc3f4d70754d18
刘文杰 <liuwenjie@chinamobile.com>
2020-12-30 16:04:40 +0800

    '测试push'

---
+++ flinkx-core/src/main/java/com/dtstack/flinkx/trans/A.java
@@ -0,0 +1,11 @@
+package com.dtstack.flinkx.trans;
+
+/**
+ * @author liuwenjie
+ * @date 2020/12/30 16:03
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("测试fork push 。。");
+    }
+}
