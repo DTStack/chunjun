377b4f591f5b3e1a7987c257d1ed9b1a050d22a0
shitou <shitou@dtstack.com>
2021-01-07 15:18:11 +0800

    [opt-34164]删除给url赋值的多余参数，使得Phoenix、Phoenix5离线采集插件支持不配置zk端口

--- flinkx-phoenix5/flinkx-phoenix5-writer/src/main/java/com/dtstack/flinkx/phoenix5/writer/Phoenix5Writer.java
+++ flinkx-phoenix5/flinkx-phoenix5-writer/src/main/java/com/dtstack/flinkx/phoenix5/writer/Phoenix5Writer.java
@@ -38,7 +38,6 @@ public class Phoenix5Writer extends JdbcDataWriter {
     public Phoenix5Writer(DataTransferConfig config) {
         super(config);
         setDatabaseInterface(new PhoenixMeta());
-      //  dbUrl = DbUtil.formatJdbcUrl(dbUrl, Collections.singletonMap("zeroDateTimeBehavior", "convertToNull"));
     }
 
     @Override
