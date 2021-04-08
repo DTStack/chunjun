a7f7a181f4512a9d4af0dbbf13fcaaaa5a2953a3
wuren <wuren@dtstack.com>
2021-04-08 16:13:13 +0800

    [connector][jdbc][mysql] fusion sync and calc connector

--- flinkx-connectors/flinkx-connector-jdbc-base/src/main/java/com/dtstack/flinkx/connector/jdbc/DtJdbcOutputFormat.java
+++ flinkx-connectors/flinkx-connector-jdbc-base/src/main/java/com/dtstack/flinkx/connector/jdbc/DtJdbcOutputFormat.java
@@ -1,13 +1,9 @@
 package com.dtstack.flinkx.connector.jdbc;
 
-import com.dtstack.flinkx.connector.jdbc.sync.DbUtil;
-
 import com.dtstack.flinkx.util.ClassUtil;
 
 import org.apache.flink.api.common.io.RichOutputFormat;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.connector.jdbc.internal.AbstractJdbcOutputFormat;
-import org.apache.flink.connector.jdbc.internal.connection.JdbcConnectionProvider;
 
 import java.io.IOException;
 import java.sql.Connection;
@@ -37,12 +33,6 @@ public class DtJdbcOutputFormat extends RichOutputFormat {
 
     @Override
     public void open(int taskNumber, int numTasks)  {
-        try {
-            ClassUtil.forName(driverName, getClass().getClassLoader());
-            dbConn = DbUtil.getConnection(dbUrl, username, password);
-        } catch (SQLException throwables) {
-            throw new IllegalArgumentException("open() failed.", throwables);
-        }
 
     }
 
