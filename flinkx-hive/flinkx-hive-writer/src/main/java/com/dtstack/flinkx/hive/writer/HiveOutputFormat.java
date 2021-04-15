c9d978f8b5cc17fbbbe6f8eaa584a3f04e15270d
dujie <2774584057@qq.com>
2021-04-15 16:33:29 +0800

    [hotfix-36332][flinkx-core][hivewriter]解决gsonutil解析到18446744073702197459出错

--- flinkx-hive/flinkx-hive-writer/src/main/java/com/dtstack/flinkx/hive/writer/HiveOutputFormat.java
+++ flinkx-hive/flinkx-hive-writer/src/main/java/com/dtstack/flinkx/hive/writer/HiveOutputFormat.java
@@ -30,6 +30,7 @@ import com.dtstack.flinkx.outputformat.BaseRichOutputFormat;
 import com.dtstack.flinkx.restore.FormatState;
 import com.dtstack.flinkx.util.ExceptionUtil;
 import com.dtstack.flinkx.util.GsonUtil;
+import com.dtstack.flinkx.util.MapUtil;
 import com.google.gson.JsonSyntaxException;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.math3.util.Pair;
@@ -194,7 +195,7 @@ public class HiveOutputFormat extends BaseRichOutputFormat {
                     event = (Map) tempObj;
                 } else if (tempObj instanceof String) {
                     try {
-                        event = GsonUtil.GSON.fromJson((String) tempObj, GsonUtil.gsonMapTypeToken);
+                        event = MapUtil.jsonStrToObject((String) tempObj,Map.class);
                     }catch (JsonSyntaxException e){
                         // is not a json string
                         //tempObj 不是map类型 则event直接往下传递
