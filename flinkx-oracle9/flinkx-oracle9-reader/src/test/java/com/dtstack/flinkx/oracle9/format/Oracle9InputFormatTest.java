ecafc74ecabe9de1a2f8b0f5d0afd5102e59e4f5
dujie <2774584057@qq.com>
2021-05-17 15:33:22 +0800

    【feat-1593】[oracle9]增加License

--- flinkx-oracle9/flinkx-oracle9-reader/src/test/java/com/dtstack/flinkx/oracle9/format/Oracle9InputFormatTest.java
+++ flinkx-oracle9/flinkx-oracle9-reader/src/test/java/com/dtstack/flinkx/oracle9/format/Oracle9InputFormatTest.java
@@ -1,10 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.dtstack.flinkx.oracle9.format;
 
 import com.dtstack.flinkx.util.ReflectionUtils;
-import oracle.xdb.XMLType;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.flink.runtime.execution.librarycache.FlinkUserCodeClassLoaders;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
@@ -12,8 +28,6 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import sun.misc.URLClassPath;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.net.URL;
 import java.net.URLClassLoader;
