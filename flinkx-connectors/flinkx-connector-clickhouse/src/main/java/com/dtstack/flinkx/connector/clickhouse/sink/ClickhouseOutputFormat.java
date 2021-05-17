a1600f6ec0bfddb3d595e5f012008f271220d057
xiuzhu <xiuzhu@dtstack.com>
2021-05-17 16:33:37 +0800

    [feat-886][clickhouse] add clickhouse

---
+++ flinkx-connectors/flinkx-connector-clickhouse/src/main/java/com/dtstack/flinkx/connector/clickhouse/sink/ClickhouseOutputFormat.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.flinkx.connector.clickhouse.sink;
+
+import com.dtstack.flinkx.connector.clickhouse.converter.ClickhouseRawTypeConverter;
+import com.dtstack.flinkx.connector.clickhouse.util.ClickhouseUtil;
+import com.dtstack.flinkx.connector.jdbc.sink.JdbcOutputFormat;
+import com.dtstack.flinkx.util.TableUtil;
+
+import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.RowType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @program: flinkx
+ * @author: xiuzhu
+ * @create: 2021/05/10
+ */
+
+public class ClickhouseOutputFormat extends JdbcOutputFormat {
+
+    protected static final Logger LOG = LoggerFactory.getLogger(ClickhouseOutputFormat.class);
+
+    @Override
+    protected void openInternal(int taskNumber, int numTasks) {
+        super.openInternal(taskNumber, numTasks);
+        try {
+            LogicalType rowType =
+                    TableUtil.createRowType(
+                            fullColumn, fullColumnType, ClickhouseRawTypeConverter::apply);
+            setRowConverter(jdbcDialect.getColumnConverter((RowType) rowType));
+        } catch (SQLException e) {
+            LOG.error("", e);
+        }
+    }
+
+    @Override
+    protected Connection getConnection() {
+        try {
+            return ClickhouseUtil.getConnection(
+                    jdbcConf.getJdbcUrl(),
+                    jdbcConf.getUsername(),
+                    jdbcConf.getPassword());
+        } catch (SQLException e) {
+            LOG.error("", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+}
