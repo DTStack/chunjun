f810f59bd30867790b6ce3e3014131e18187a043
zhongjiang.mu <mzjnumber1@163.com>
2021-05-24 18:10:25 +0800

    [hotfix-394][kb] 解决earliest-offset不生效问题

--- flinkx-kb/flinkx-kb-core/src/main/java/com/dtstack/flinkx/kafkabase/util/KafkaUtil.java
+++ flinkx-kb/flinkx-kb-core/src/main/java/com/dtstack/flinkx/kafkabase/util/KafkaUtil.java
@@ -95,6 +95,7 @@ public class KafkaUtil {
         switch (mode){
             case EARLIEST:
                 props.put("auto.offset.reset", "earliest");
+                break;
             case LATEST:
                 props.put("auto.offset.reset", "latest");
         }
