6b92a3171cb1c8f7ed83703e2793edec42e85f12
fengjiangtao <fengjiangtao_yewu@cmss.chinamobile.com>
2021-06-08 11:12:19 +0800

    Code Source From: Self Code Description: 调整PulsarReader参数 PR: #385

--- flinkx-pulsar/flinkx-pulsar-reader/src/main/java/com/dtstack/flinkx/pulsar/format/PulsarInputFormat.java
+++ flinkx-pulsar/flinkx-pulsar-reader/src/main/java/com/dtstack/flinkx/pulsar/format/PulsarInputFormat.java
@@ -40,6 +40,7 @@ public class PulsarInputFormat extends BaseRichInputFormat {
     protected String fieldDelimiter;
     protected boolean blankIgnore;
     protected int timeout;
+    protected String pulsarServiceUrl;
     protected List<MetaColumn> metaColumns;
     protected Map<String, Object> consumerSettings;
 
@@ -148,6 +149,10 @@ public class PulsarInputFormat extends BaseRichInputFormat {
 
     public int getTimeout() {
         return timeout;
+    }
+
+    public String getPulsarServiceUrl() {
+        return pulsarServiceUrl;
     }
 
     public Map<String, Object> getConsumerSettings() {
