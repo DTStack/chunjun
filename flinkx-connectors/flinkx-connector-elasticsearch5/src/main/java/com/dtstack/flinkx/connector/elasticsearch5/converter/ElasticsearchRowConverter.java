07818f63e5ea2d63061717143c3270b7819bc9e0
HiLany <lanyang@pivotal.net.cn>
2021-06-28 21:57:13 +0800

    [feat-2153][elasticsearch5]add license.

--- flinkx-connectors/flinkx-connector-elasticsearch5/src/main/java/com/dtstack/flinkx/connector/elasticsearch5/converter/ElasticsearchRowConverter.java
+++ flinkx-connectors/flinkx-connector-elasticsearch5/src/main/java/com/dtstack/flinkx/connector/elasticsearch5/converter/ElasticsearchRowConverter.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.dtstack.flinkx.connector.elasticsearch5.converter;
 
 import org.apache.flink.table.data.DecimalData;
@@ -40,6 +58,7 @@ import scala.Tuple3;
  * @create: 2021/06/27 23:48
  */
 public class ElasticsearchRowConverter extends AbstractRowConverter<Map<String, Object>, Map<String, Object>, Map<String, Object>, LogicalType> {
+
     private static final long serialVersionUID = 2L;
 
     private Logger LOG = LoggerFactory.getLogger(ElasticsearchRowConverter.class);
