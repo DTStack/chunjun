0ec26617ee4b894b7649e15a1b54d00040ad699f
chuixue <chuixue@dtstack.com>
2021-07-09 18:01:00 +0800

    [fix][example]fix example

--- flinkx-examples/sql/mysql/mysql_hdfs_polling.sql
+++ flinkx-examples/sql/mysql/mysql_hdfs_polling.sql
@@ -19,15 +19,15 @@ CREATE TABLE source
 --       ,'scan.query-timeout' = '10' -- 数据库连接超时时间。默认：不超时
 
       ,'scan.partition.column' = 'id' -- 多并行度读取的切分字段，多并行度下必需要设置。无默认
-      ,'scan.partition.strategy' = 'mod' -- 数据分片策略。默认：range
+      ,'scan.partition.strategy' = 'mod' -- 数据分片策略。默认：range，如果并行度大于1，且是增量任务或者间隔轮询，则会使用mod分片
 
       ,'scan.increment.column' = 'id' -- 增量字段名称，必须是表中的字段。非必填，无默认
 --       ,'scan.increment.column-type' = 'int'  -- 增量字段类型。非必填，无默认
-      ,'scan.start-location' = '100' -- 增量字段开始位置,如果不指定则先同步所有，然后在增量。非必填，无默认
+      ,'scan.start-location' = '100' -- 增量字段开始位置,如果不指定则先同步所有，然后在增量。非必填，无默认，如果没配置scan.increment.column，则不生效
 
       ,'scan.restore.columnname' = 'id' -- 开启了cp，任务从sp/cp续跑字段名称。如果续跑，则会覆盖scan.start-location开始位置，从续跑点开始。非必填，无默认
 --       ,'scan.restore.columntype' = 'int' -- 开启了cp，任务从sp/cp续跑字段类型。非必填，无默认
-);
+      );
 
 CREATE TABLE sink
 (
@@ -56,7 +56,7 @@ CREATE TABLE sink
       ,'fileType' = 'text'
 
       ,'sink.parallelism' = '3'
-);
+      );
 
 insert into sink
 select *
