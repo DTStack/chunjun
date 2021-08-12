51bf33ca8ff72c8680d8de04d186d5f86ac0402b
wuren <wuren@dtstack.com>
2021-08-12 20:33:27 +0800

    [bugfix][postgresql] postgresql history bug

--- flinkx-connectors/flinkx-connector-postgresql/src/main/java/com/dtstack/flinkx/connector/postgresql/sink/PostgresqlSinkFactory.java
+++ flinkx-connectors/flinkx-connector-postgresql/src/main/java/com/dtstack/flinkx/connector/postgresql/sink/PostgresqlSinkFactory.java
@@ -19,6 +19,7 @@
 package com.dtstack.flinkx.connector.postgresql.sink;
 
 import com.dtstack.flinkx.conf.SyncConf;
+import com.dtstack.flinkx.connector.jdbc.sink.JdbcOutputFormatBuilder;
 import com.dtstack.flinkx.connector.jdbc.sink.JdbcSinkFactory;
 import com.dtstack.flinkx.connector.postgresql.dialect.PostgresqlDialect;
 
@@ -33,5 +34,10 @@ public class PostgresqlSinkFactory extends JdbcSinkFactory {
 
     public PostgresqlSinkFactory(SyncConf syncConf) {
         super(syncConf, new PostgresqlDialect());
+    }
+
+    @Override
+    protected JdbcOutputFormatBuilder getBuilder() {
+        return new JdbcOutputFormatBuilder(new PostgresOutputFormat());
     }
 }
