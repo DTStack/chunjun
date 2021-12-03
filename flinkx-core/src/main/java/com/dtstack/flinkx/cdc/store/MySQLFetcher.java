66e98ccf3f7bd57c0138a3711f0ee22eb071e8ea
tiezhu <tiezhu@dtstack.com>
2021-12-03 12:03:58 +0800

    [cdc-restore] code monitor.

--- flinkx-core/src/main/java/com/dtstack/flinkx/cdc/store/MySQLFetcher.java
+++ flinkx-core/src/main/java/com/dtstack/flinkx/cdc/store/MySQLFetcher.java
@@ -2,6 +2,8 @@ package com.dtstack.flinkx.cdc.store;
 
 import org.apache.flink.table.data.RowData;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author tiezhu@dtstack.com
  * @since 2021/12/2 星期四
@@ -10,6 +12,21 @@ public class MySQLFetcher extends Fetcher {
 
     @Override
     public boolean fetch(RowData data) {
-        return false;
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException ignore) {
+        }
+        System.out.println("Data 已经被处理");
+        return true;
+    }
+
+    @Override
+    public void open() {
+        System.out.println("MySQL Fetcher open()");
+    }
+
+    @Override
+    public void close() {
+        System.out.println("MySQL Fetcher close()");
     }
 }
--- flinkx-core/src/main/java/com/dtstack/flinkx/cdc/store/MySQLStore.java
+++ flinkx-core/src/main/java/com/dtstack/flinkx/cdc/store/MySQLStore.java
@@ -6,7 +6,20 @@ import org.apache.flink.table.data.RowData;
  * @author tiezhu@dtstack.com
  * @since 2021/12/2 星期四
  */
-public class MySQLStore implements Store {
+public class MySQLStore extends Store {
+
     @Override
-    public void store(RowData data) {}
+    public void store(RowData data) {
+        System.out.println("存储data到外部存储");
+    }
+
+    @Override
+    public void open() {
+        System.out.println("MySQL Store open()");
+    }
+
+    @Override
+    public void closeSubclass() {
+        System.out.println("MySQL Store closed.");
+    }
 }
