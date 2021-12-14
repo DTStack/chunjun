b2fa49db0cd78b76906e53093f633e97c6550d0c
tiezhu <tiezhu@dtstack.com>
2021-12-14 11:08:29 +0800

    [feat][mapping] Add mapping.

---
+++ flinkx-core/src/main/java/com/dtstack/flinkx/mapping/FieldMapping.java
@@ -0,0 +1,12 @@
+package com.dtstack.flinkx.mapping;
+
+import java.io.Serializable;
+
+/**
+ * @author tiezhu@dtstack.com
+ * @since 2021/12/14 星期二
+ */
+public class FieldMapping implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+}
