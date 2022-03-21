10afa16f11f2fce06be7a80f5853eb251ab475ad
xirang@dtstack.com <xirang@dtstack.com>
2022-03-21 15:28:10 +0800

    [feat-4789][influxdb] code format

--- flinkx-connectors/flinkx-connector-influxdb/src/main/java/com/dtstack/flinkx/connector/influxdb/sink/InfluxdbSinkFactory.java
+++ flinkx-connectors/flinkx-connector-influxdb/src/main/java/com/dtstack/flinkx/connector/influxdb/sink/InfluxdbSinkFactory.java
@@ -1,26 +1,32 @@
 package com.dtstack.flinkx.connector.influxdb.sink;
 
 import com.dtstack.flinkx.conf.SyncConf;
-import com.dtstack.flinkx.connector.influxdb.conf.InfluxdbConfig;
+import com.dtstack.flinkx.connector.influxdb.conf.InfluxdbSinkConfig;
 import com.dtstack.flinkx.connector.influxdb.converter.InfluxdbRawTypeConverter;
 import com.dtstack.flinkx.converter.RawTypeConverter;
 import com.dtstack.flinkx.sink.SinkFactory;
-import com.dtstack.flinkx.util.JsonUtil;
+import com.dtstack.flinkx.util.GsonUtil;
 
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
 import org.apache.flink.table.data.RowData;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Map;
+
 /** @Author xirang @Company Dtstack @Date: 2022/3/14 9:56 AM */
 public class InfluxdbSinkFactory extends SinkFactory {
 
-    protected InfluxdbConfig influxdbConfig;
+    protected InfluxdbSinkConfig influxdbConfig;
 
     public InfluxdbSinkFactory(SyncConf syncConf) {
         super(syncConf);
-        influxdbConfig =
-                JsonUtil.toObject(
-                        JsonUtil.toJson(syncConf.getWriter().getParameter()), InfluxdbConfig.class);
+        Map<String, Object> parameter = syncConf.getJob().getWriter().getParameter();
+        Gson gson = new GsonBuilder().create();
+        GsonUtil.setTypeAdapter(gson);
+        this.influxdbConfig = gson.fromJson(gson.toJson(parameter), InfluxdbSinkConfig.class);
         influxdbConfig.setColumn(syncConf.getWriter().getFieldList());
         super.initFlinkxCommonConf(influxdbConfig);
     }
@@ -34,10 +40,9 @@ public class InfluxdbSinkFactory extends SinkFactory {
     public DataStreamSink<RowData> createSink(DataStream<RowData> dataSet) {
         InfluxdbOutputFormatBuilder builder = new InfluxdbOutputFormatBuilder();
         builder.setInfluxdbConfig(influxdbConfig);
-        builder.setColumnMetaInfos(influxdbConfig.getColumn());
         builder.setDatabase(influxdbConfig.getDatabase());
         builder.setMeasurement(influxdbConfig.getMeasurement());
-        //        new InfluxdbColumnConverter()
+        builder.setEnableBatch(influxdbConfig.isEnableBatch());
         return createOutput(dataSet, builder.finish());
     }
 }
