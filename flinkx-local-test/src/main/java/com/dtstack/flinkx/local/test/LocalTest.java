e6f58668763d017189b10906968d2211f7abb819
dujie <dujie@dtstack.com>
2022-03-23 14:27:08 +0800

    [feat-692][Hive]modify support schema

--- flinkx-local-test/src/main/java/com/dtstack/flinkx/local/test/LocalTest.java
+++ flinkx-local-test/src/main/java/com/dtstack/flinkx/local/test/LocalTest.java
@@ -48,14 +48,14 @@ public class LocalTest {
     public static void main(String[] args) throws Exception {
         LOG.warn("-----");
         Properties confProperties = new Properties();
-//        confProperties.setProperty("flink.checkpoint.interval", "30000");
-//        confProperties.setProperty("state.backend","ROCKSDB");
-//        confProperties.setProperty("state.checkpoints.num-retained", "10");
-//        confProperties.setProperty("state.checkpoints.dir", "file:///ck");
+        //        confProperties.setProperty("flink.checkpoint.interval", "30000");
+        //        confProperties.setProperty("state.backend","ROCKSDB");
+        //        confProperties.setProperty("state.checkpoints.num-retained", "10");
+        //        confProperties.setProperty("state.checkpoints.dir", "file:///ck");
         String userDir = System.getProperty("user.dir");
 
-        String jobPath =
-                userDir + "/flinkx-examples/json/stream/stream.json";
+        String jobPath = "/Users/wtz/work_place/job_place/json/1.12/binlog-stream.json";
+        //                userDir + "/flinkx-examples/json/stream/stream.json";
         String flinkxDistDir = userDir + "/flinkx-dist";
         String s = "";
 
@@ -69,42 +69,43 @@ public class LocalTest {
             argsList.add("sync");
             argsList.add("-job");
             argsList.add(URLEncoder.encode(content, StandardCharsets.UTF_8.name()));
-//            argsList.add("-flinkConfDir");
-//            argsList.add("/opt/dtstack/flink-1.12.2/conf/");
-//            argsList.add("-confProp");
-//            // 脏数据相关配置信息
-//            StringBuilder stringBuilder = new StringBuilder();
-//            stringBuilder
-//                    .append("{")
-//                    .append("\"flinkx.dirty-data.output-type\":\"print\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.max-rows\":\"1000\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.max-collect-failed-rows\":\"100\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.url\":\"jdbc:mysql://localhost:3306/tiezhu\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.username\":\"root\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.password\":\"abc123\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.database\":\"tiezhu\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.table\":\"flinkx_dirty_data\"")
-//                    .append(",")
-//                    .append("\"flinkx.dirty-data.jdbc.batch-size\":\"10\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.log.print-interval\":\"10\"")
-//                    .append("}");
-//            argsList.add(stringBuilder.toString());
+            //            argsList.add("-flinkConfDir");
+            //            argsList.add("/opt/dtstack/flink-1.12.2/conf/");
+            //            argsList.add("-confProp");
+            //            // 脏数据相关配置信息
+            //            StringBuilder stringBuilder = new StringBuilder();
+            //            stringBuilder
+            //                    .append("{")
+            //                    .append("\"flinkx.dirty-data.output-type\":\"print\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.max-rows\":\"1000\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.max-collect-failed-rows\":\"100\"")
+            //                    .append(", ")
+            //
+            // .append("\"flinkx.dirty-data.jdbc.url\":\"jdbc:mysql://localhost:3306/tiezhu\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.jdbc.username\":\"root\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.jdbc.password\":\"abc123\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.jdbc.database\":\"tiezhu\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.jdbc.table\":\"flinkx_dirty_data\"")
+            //                    .append(",")
+            //                    .append("\"flinkx.dirty-data.jdbc.batch-size\":\"10\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.log.print-interval\":\"10\"")
+            //                    .append("}");
+            //            argsList.add(stringBuilder.toString());
             argsList.add(GsonUtil.GSON.toJson(confProperties));
         } else if (StringUtils.endsWith(jobPath, "sql")) {
             argsList.add("-jobType");
             argsList.add("sql");
             argsList.add("-job");
             argsList.add(URLEncoder.encode(content, StandardCharsets.UTF_8.name()));
-//            argsList.add("-flinkConfDir");
-//            argsList.add("/opt/dtstack/flink-1.12.2/conf/");
+            //            argsList.add("-flinkConfDir");
+            //            argsList.add("/opt/dtstack/flink-1.12.2/conf/");
             argsList.add("-jobName");
             argsList.add("flinkStreamSQLLocalTest");
             argsList.add("-flinkxDistDir");
@@ -113,51 +114,60 @@ public class LocalTest {
             argsList.add(flinkxDistDir);
             argsList.add("-pluginLoadMode");
             argsList.add("LocalTest");
-//            argsList.add("-confProp");
-//            // 脏数据相关配置信息
-//            StringBuilder stringBuilder = new StringBuilder();
-//            stringBuilder
-//                    .append("{")
-//                    .append("\"flinkx.dirty-data.output-type\":\"mysql\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.max-rows\":\"1000\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.max-collect-failed-rows\":\"100\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.url\":\"jdbc:mysql://localhost:3306/tiezhu\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.username\":\"root\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.password\":\"abc123\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.database\":\"tiezhu\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.jdbc.table\":\"flinkx_dirty_data\"")
-//                    .append(",")
-//                    .append("\"flinkx.dirty-data.jdbc.batch-size\":\"10\"")
-//                    .append(", ")
-//                    .append("\"flinkx.dirty-data.log.print-interval\":\"10\"")
-//                    .append("}");
-//            argsList.add(stringBuilder.toString());
-//            argsList.add("-confProp");
-//            argsList.add("{\"execution.checkpointing.interval\":\"60000\"}");
-//            argsList.add("{\"sql.checkpoint.mode\":\"AT_LEAST_ONCE\",\"flink.checkpoint.interval\":\"300000\"}");
-//            argsList.add("{\"sql.checkpoint.mode\":\"EXACTLY_ONCE\",\"flink.checkpoint.interval\":\"300000\"}");
-//            argsList.add("{\"sql.env.parallelism\":\"2\",\"metrics.latency.interval\":\"30000\",\"metrics.latency.granularity\":\"operator\",\"time.characteristic\":\"eventTime\",\"state.backend\":\"FILESYSTEM\",\"state.checkpoints.dir\":\"hdfs://ns1/dtInsight/flink110/savepoints/POC_MEIDI_STREAM_JOIN\",\"sql.ttl.min\":\"5m\",\"sql.ttl.max\":\"10m\",\"flink.checkpoint.interval\":\"300000\",\"sql.checkpoint.mode\":\"EXACTLY_ONCE\",\"sql.checkpoint.timeout\":\"200000\",\"sql.max.concurrent.checkpoints\":\"1\",\"sql.checkpoint.cleanup.mode\":\"true\",\"timezone\":\"Asia/Shanghai\",\"early.trigger\":\"1\"}");
-//            argsList.add("{\"sql.env.parallelism\":\"2\",\"metrics.latency.interval\":\"30000\",\"metrics.latency.granularity\":\"operator\",\"time.characteristic\":\"eventTime\",\"sql.ttl.min\":\"5m\",\"sql.ttl.max\":\"10m\",\"flink.checkpoint.interval\":\"300000\",\"sql.checkpoint.mode\":\"EXACTLY_ONCE\",\"sql.checkpoint.timeout\":\"200000\",\"sql.max.concurrent.checkpoints\":\"1\",\"sql.checkpoint.cleanup.mode\":\"true\",\"timezone\":\"Asia/Shanghai\",\"early.trigger\":\"1\"}");
+            //            argsList.add("-confProp");
+            //            // 脏数据相关配置信息
+            //            StringBuilder stringBuilder = new StringBuilder();
+            //            stringBuilder
+            //                    .append("{")
+            //                    .append("\"flinkx.dirty-data.output-type\":\"mysql\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.max-rows\":\"1000\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.max-collect-failed-rows\":\"100\"")
+            //                    .append(", ")
+            //
+            // .append("\"flinkx.dirty-data.jdbc.url\":\"jdbc:mysql://localhost:3306/tiezhu\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.jdbc.username\":\"root\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.jdbc.password\":\"abc123\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.jdbc.database\":\"tiezhu\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.jdbc.table\":\"flinkx_dirty_data\"")
+            //                    .append(",")
+            //                    .append("\"flinkx.dirty-data.jdbc.batch-size\":\"10\"")
+            //                    .append(", ")
+            //                    .append("\"flinkx.dirty-data.log.print-interval\":\"10\"")
+            //                    .append("}");
+            //            argsList.add(stringBuilder.toString());
+            //            argsList.add("-confProp");
+            //            argsList.add("{\"execution.checkpointing.interval\":\"60000\"}");
+            //
+            // argsList.add("{\"sql.checkpoint.mode\":\"AT_LEAST_ONCE\",\"flink.checkpoint.interval\":\"300000\"}");
+            //
+            // argsList.add("{\"sql.checkpoint.mode\":\"EXACTLY_ONCE\",\"flink.checkpoint.interval\":\"300000\"}");
+            //
+            // argsList.add("{\"sql.env.parallelism\":\"2\",\"metrics.latency.interval\":\"30000\",\"metrics.latency.granularity\":\"operator\",\"time.characteristic\":\"eventTime\",\"state.backend\":\"FILESYSTEM\",\"state.checkpoints.dir\":\"hdfs://ns1/dtInsight/flink110/savepoints/POC_MEIDI_STREAM_JOIN\",\"sql.ttl.min\":\"5m\",\"sql.ttl.max\":\"10m\",\"flink.checkpoint.interval\":\"300000\",\"sql.checkpoint.mode\":\"EXACTLY_ONCE\",\"sql.checkpoint.timeout\":\"200000\",\"sql.max.concurrent.checkpoints\":\"1\",\"sql.checkpoint.cleanup.mode\":\"true\",\"timezone\":\"Asia/Shanghai\",\"early.trigger\":\"1\"}");
+            //
+            // argsList.add("{\"sql.env.parallelism\":\"2\",\"metrics.latency.interval\":\"30000\",\"metrics.latency.granularity\":\"operator\",\"time.characteristic\":\"eventTime\",\"sql.ttl.min\":\"5m\",\"sql.ttl.max\":\"10m\",\"flink.checkpoint.interval\":\"300000\",\"sql.checkpoint.mode\":\"EXACTLY_ONCE\",\"sql.checkpoint.timeout\":\"200000\",\"sql.max.concurrent.checkpoints\":\"1\",\"sql.checkpoint.cleanup.mode\":\"true\",\"timezone\":\"Asia/Shanghai\",\"early.trigger\":\"1\"}");
 
             /*
-                python.files                             -pyfs
-                python.client.executable                [flink configuration] -> [table api] -> [environment variable PYFLINK_CLIENT_EXECUTABLE]
-                python.executable                       -pyexec
-                python.requirements                     -pyreq
-             */
+               python.files                             -pyfs
+               python.client.executable                [flink configuration] -> [table api] -> [environment variable PYFLINK_CLIENT_EXECUTABLE]
+               python.executable                       -pyexec
+               python.requirements                     -pyreq
+            */
             /* ---------------------------------------- pyFlink 测试 start --------------------------------------- */
-            Map<String,String> config = new HashMap<>();
-            config.put("python.files","/Users/lzq/Desktop/Projects/Flink/PyFlinkDemo/enjoyment.code/PythonUDFProvideToJava/test1.py");
-            config.put("python.client.executable","python3");
-            config.put("python.executable","python3");
-            config.put("python.requirements","/Users/lzq/Desktop/Projects/Flink/PyFlinkDemo/enjoyment.code/PythonUDFProvideToJava/requirements3.txt#/Users/lzq/Desktop/Projects/Flink/PyFlinkDemo/enjoyment.code/PythonUDFProvideToJava/cached_dir_binary3");
+            Map<String, String> config = new HashMap<>();
+            config.put(
+                    "python.files",
+                    "/Users/lzq/Desktop/Projects/Flink/PyFlinkDemo/enjoyment.code/PythonUDFProvideToJava/test1.py");
+            config.put("python.client.executable", "python3");
+            config.put("python.executable", "python3");
+            config.put(
+                    "python.requirements",
+                    "/Users/lzq/Desktop/Projects/Flink/PyFlinkDemo/enjoyment.code/PythonUDFProvideToJava/requirements3.txt#/Users/lzq/Desktop/Projects/Flink/PyFlinkDemo/enjoyment.code/PythonUDFProvideToJava/cached_dir_binary3");
             String configJsonString = GsonUtil.GSON.toJson(config);
             argsList.add("-confProp");
             argsList.add(configJsonString);
