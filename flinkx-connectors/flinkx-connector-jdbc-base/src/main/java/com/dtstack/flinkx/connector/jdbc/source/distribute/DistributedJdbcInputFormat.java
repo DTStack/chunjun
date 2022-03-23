cd6fbcfef4f5be74c40eab122534345d19a6f1f8
dujie <dujie@dtstack.com>
2022-03-23 23:29:41 +0800

    [feat-4791][mysql]modify support schema

--- flinkx-connectors/flinkx-connector-jdbc-base/src/main/java/com/dtstack/flinkx/connector/jdbc/source/distribute/DistributedJdbcInputFormat.java
+++ flinkx-connectors/flinkx-connector-jdbc-base/src/main/java/com/dtstack/flinkx/connector/jdbc/source/distribute/DistributedJdbcInputFormat.java
@@ -160,6 +160,7 @@ public class DistributedJdbcInputFormat extends JdbcInputFormat {
         jdbcConf.setUsername(currentSource.getUserName());
         jdbcConf.setPassword(currentSource.getPassword());
         jdbcConf.setTable(currentSource.getTable());
+        jdbcConf.setSchema(currentSource.getSchema());
         return JdbcUtil.getConnection(jdbcConf, this.jdbcDialect);
     }
 
