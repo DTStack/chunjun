a673a743407c6c71d657450806b067e07f01a0de
zhao_wei_nan <326747337@qq.com>
2022-05-23 12:18:49 +0800

    [hotfix-827][jdbc] jdbc PreparedStmtProxy can't resolve DELETE statement.this is make flink retraction failed.

--- chunjun-connectors/chunjun-connector-jdbc-base/src/main/java/com/dtstack/chunjun/connector/jdbc/sink/DynamicPreparedStmt.java
+++ chunjun-connectors/chunjun-connector-jdbc-base/src/main/java/com/dtstack/chunjun/connector/jdbc/sink/DynamicPreparedStmt.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.dtstack.chunjun.connector.jdbc.sink;
 
 import com.dtstack.chunjun.conf.FieldConf;
@@ -24,6 +25,7 @@ import com.dtstack.chunjun.connector.jdbc.statement.FieldNamedPreparedStatement;
 import com.dtstack.chunjun.connector.jdbc.statement.FieldNamedPreparedStatementImpl;
 import com.dtstack.chunjun.connector.jdbc.util.JdbcUtil;
 import com.dtstack.chunjun.converter.AbstractRowConverter;
+import com.dtstack.chunjun.enums.EWriteMode;
 import com.dtstack.chunjun.util.TableUtil;
 
 import org.apache.flink.table.types.logical.RowType;
@@ -71,6 +73,7 @@ public class DynamicPreparedStmt {
             RowKind rowKind,
             Connection connection,
             JdbcDialect jdbcDialect,
+            JdbcConf jdbcConf,
             boolean writeExtInfo)
             throws SQLException {
         DynamicPreparedStmt dynamicPreparedStmt = new DynamicPreparedStmt();
@@ -81,7 +84,14 @@ public class DynamicPreparedStmt {
         dynamicPreparedStmt.getColumnMeta(schemaName, tableName, connection);
         dynamicPreparedStmt.buildRowConvert();
 
-        String sql = dynamicPreparedStmt.prepareTemplates(rowKind, schemaName, tableName);
+        String sql =
+                dynamicPreparedStmt.prepareTemplates(
+                        rowKind,
+                        schemaName,
+                        tableName,
+                        jdbcConf.getUniqueKey().toArray(new String[0]),
+                        jdbcConf.getMode(),
+                        jdbcConf.isAllReplace());
         String[] fieldNames = new String[dynamicPreparedStmt.columnNameList.size()];
         dynamicPreparedStmt.columnNameList.toArray(fieldNames);
         dynamicPreparedStmt.fieldNamedPreparedStatement =
@@ -95,12 +105,14 @@ public class DynamicPreparedStmt {
             RowKind rowKind,
             Connection connection,
             JdbcDialect jdbcDialect,
-            List<FieldConf> fieldConfList,
+            JdbcConf jdbcConf,
             AbstractRowConverter<?, ?, ?, ?> rowConverter)
             throws SQLException {
+        List<FieldConf> fieldConfList = jdbcConf.getColumn();
         DynamicPreparedStmt dynamicPreparedStmt = new DynamicPreparedStmt();
         dynamicPreparedStmt.jdbcDialect = jdbcDialect;
         dynamicPreparedStmt.rowConverter = rowConverter;
+        dynamicPreparedStmt.jdbcConf = jdbcConf;
         String[] fieldNames = new String[fieldConfList.size()];
         for (int i = 0; i < fieldConfList.size(); i++) {
             FieldConf fieldConf = fieldConfList.get(i);
@@ -108,7 +120,14 @@ public class DynamicPreparedStmt {
             dynamicPreparedStmt.columnNameList.add(fieldConf.getName());
             dynamicPreparedStmt.columnTypeList.add(fieldConf.getType());
         }
-        String sql = dynamicPreparedStmt.prepareTemplates(rowKind, schemaName, tableName);
+        String sql =
+                dynamicPreparedStmt.prepareTemplates(
+                        rowKind,
+                        schemaName,
+                        tableName,
+                        jdbcConf.getUniqueKey().toArray(new String[0]),
+                        jdbcConf.getMode(),
+                        jdbcConf.isAllReplace());
         dynamicPreparedStmt.fieldNamedPreparedStatement =
                 FieldNamedPreparedStatementImpl.prepareStatement(connection, sql, fieldNames);
         return dynamicPreparedStmt;
@@ -131,14 +150,20 @@ public class DynamicPreparedStmt {
         return dynamicPreparedStmt;
     }
 
-    protected String prepareTemplates(RowKind rowKind, String schemaName, String tableName) {
+    protected String prepareTemplates(
+            RowKind rowKind,
+            String schemaName,
+            String tableName,
+            String[] uniqueKeys,
+            String mode,
+            boolean allReplace) {
         String singleSql = null;
         switch (rowKind) {
             case INSERT:
             case UPDATE_AFTER:
                 singleSql =
-                        jdbcDialect.getInsertIntoStatement(
-                                schemaName, tableName, columnNameList.toArray(new String[0]));
+                        this.getInsertStatementWithWriteMode(
+                                mode, schemaName, tableName, uniqueKeys, allReplace);
                 break;
             case DELETE:
             case UPDATE_BEFORE:
@@ -149,6 +174,40 @@ public class DynamicPreparedStmt {
             default:
                 // TODO 异常如何处理
                 LOG.warn("not support RowKind: {}", rowKind);
+        }
+
+        return singleSql;
+    }
+
+    protected String getInsertStatementWithWriteMode(
+            String mode,
+            String schemaName,
+            String tableName,
+            String[] uniqueKeys,
+            boolean allReplace) {
+        String singleSql;
+        if (EWriteMode.INSERT.name().equalsIgnoreCase(mode)) {
+            singleSql =
+                    jdbcDialect.getInsertIntoStatement(
+                            schemaName, tableName, columnNameList.toArray(new String[0]));
+        } else if (EWriteMode.REPLACE.name().equalsIgnoreCase(mode)) {
+            singleSql =
+                    jdbcDialect
+                            .getReplaceStatement(
+                                    schemaName, tableName, columnNameList.toArray(new String[0]))
+                            .get();
+        } else if (EWriteMode.UPDATE.name().equalsIgnoreCase(mode)) {
+            singleSql =
+                    jdbcDialect
+                            .getUpsertStatement(
+                                    schemaName,
+                                    tableName,
+                                    columnNameList.toArray(new String[0]),
+                                    uniqueKeys,
+                                    allReplace)
+                            .get();
+        } else {
+            throw new IllegalArgumentException("Unknown write mode:" + mode);
         }
 
         return singleSql;
