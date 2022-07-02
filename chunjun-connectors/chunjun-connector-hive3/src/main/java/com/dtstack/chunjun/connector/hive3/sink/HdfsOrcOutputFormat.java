991b69145b9ee440c4f64e9f24af678be824569b
liuliu <liuliu@dtstack.com>
2022-07-02 17:46:45 +0800

    [feat][hive3]add hive3 connector

---
+++ chunjun-connectors/chunjun-connector-hive3/src/main/java/com/dtstack/chunjun/connector/hive3/sink/HdfsOrcOutputFormat.java
@@ -0,0 +1,239 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.chunjun.connector.hive3.sink;
+
+import com.dtstack.chunjun.connector.hive3.enums.CompressType;
+import com.dtstack.chunjun.connector.hive3.enums.FileType;
+import com.dtstack.chunjun.connector.hive3.util.Hive3Util;
+import com.dtstack.chunjun.enums.ColumnType;
+import com.dtstack.chunjun.enums.SizeUnitType;
+import com.dtstack.chunjun.throwable.ChunJunRuntimeException;
+import com.dtstack.chunjun.throwable.WriteRecordException;
+import com.dtstack.chunjun.util.ColumnTypeUtil;
+import com.dtstack.chunjun.util.ExceptionUtil;
+import com.dtstack.chunjun.util.ReflectionUtils;
+
+import org.apache.flink.table.data.RowData;
+
+import org.apache.hadoop.hive.common.type.HiveDecimal;
+import org.apache.hadoop.hive.ql.io.orc.OrcFile;
+import org.apache.hadoop.hive.ql.io.orc.OrcSerde;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.compress.BZip2Codec;
+import org.apache.hadoop.io.compress.DefaultCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.io.compress.Lz4Codec;
+import org.apache.hadoop.io.compress.SnappyCodec;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RecordWriter;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.security.UserGroupInformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/** @author liuliu 2022/3/27 */
+public class HdfsOrcOutputFormat extends BaseHdfsOutputFormat {
+    private RecordWriter recordWriter;
+    private OrcSerde orcSerde;
+    private StructObjectInspector inspector;
+    private FileOutputFormat outputFormat;
+    private JobConf jobConf;
+    private static final ColumnTypeUtil.DecimalInfo ORC_DEFAULT_DECIMAL_INFO =
+            new ColumnTypeUtil.DecimalInfo(
+                    HiveDecimal.SYSTEM_DEFAULT_PRECISION, HiveDecimal.SYSTEM_DEFAULT_SCALE);
+
+    // kerberos 认证
+    protected transient UserGroupInformation ugi;
+    protected boolean openKerberos;
+    public static final String HADOOP_USER_NAME = "HADOOP_USER_NAME";
+    public static final String KEY_PRINCIPAL = "principal";
+
+    protected final Logger LOG = LoggerFactory.getLogger(HdfsOrcOutputFormat.class);
+
+    @Override
+    protected void openSource() {
+        super.openSource();
+        orcSerde = new OrcSerde();
+        outputFormat = new org.apache.hadoop.hive.ql.io.orc.OrcOutputFormat();
+        jobConf = new JobConf(conf);
+        FileOutputFormat.setOutputCompressorClass(jobConf, getOrcCodecClass(compressType));
+
+        List<ObjectInspector> fullColTypeList = new ArrayList<>();
+
+        decimalColInfo = new HashMap<>((fullColumnTypeList.size() << 2) / 3);
+        for (int i = 0; i < fullColumnTypeList.size(); i++) {
+            String columnType = fullColumnTypeList.get(i);
+
+            ColumnTypeUtil.DecimalInfo decimalInfo = null;
+            if (ColumnTypeUtil.isDecimalType(columnType)) {
+                decimalInfo = ColumnTypeUtil.getDecimalInfo(columnType, ORC_DEFAULT_DECIMAL_INFO);
+                decimalColInfo.put(fullColumnNameList.get(i), decimalInfo);
+            }
+
+            ColumnType type = ColumnType.getType(columnType);
+            fullColTypeList.add(Hive3Util.columnTypeToObjectInspector(type, decimalInfo));
+        }
+
+        this.inspector =
+                ObjectInspectorFactory.getStandardStructObjectInspector(
+                        fullColumnNameList, fullColTypeList);
+    }
+
+    @Override
+    protected CompressType getCompressType() {
+        return CompressType.getByTypeAndFileType(hdfsConf.getCompress(), FileType.ORC.name());
+    }
+
+    protected Class getOrcCodecClass(CompressType compressType) {
+        if (CompressType.ORC_SNAPPY.equals(compressType)) {
+            return SnappyCodec.class;
+        } else if (CompressType.ORC_BZIP.equals(compressType)) {
+            return BZip2Codec.class;
+        } else if (CompressType.ORC_GZIP.equals(compressType)) {
+            return GzipCodec.class;
+        } else if (CompressType.ORC_LZ4.equals(compressType)) {
+            return Lz4Codec.class;
+        } else {
+            return DefaultCodec.class;
+        }
+    }
+
+    @Override
+    protected void nextBlock() {
+        super.nextBlock();
+        if (recordWriter != null) {
+            return;
+        }
+        try {
+            String currentBlockTmpPath = tmpPath + File.separatorChar + currentFileName;
+            recordWriter =
+                    outputFormat.getRecordWriter(null, jobConf, currentBlockTmpPath, Reporter.NULL);
+            currentFileIndex++;
+
+            setFs();
+            LOG.info("nextBlock:Current block writer record:" + rowsOfCurrentBlock);
+            LOG.info("Current block file name:" + currentBlockTmpPath);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    Hive3Util.parseErrorMsg(null, ExceptionUtil.getErrorMessage(e)), e);
+        }
+    }
+
+    /**
+     * 数据源开启kerberos时 如果这里不通过反射对 writerOptions 赋值fs，则在recordWriter.writer时 会初始化一个fs 此fs不在ugi里获取的
+     * 导致开启了kerberos的数据源在checkpoint时进行 recordWriter.close() 操作，会出现kerberos认证错误
+     */
+    private void setFs() throws IllegalAccessException {
+        if (Hive3Util.isOpenKerberos(hdfsConf.getHadoopConfig())) {
+            Field declaredField = ReflectionUtils.getDeclaredField(recordWriter, "options");
+            assert declaredField != null;
+            declaredField.setAccessible(true);
+            OrcFile.WriterOptions writerOptions =
+                    (OrcFile.WriterOptions) declaredField.get(recordWriter);
+            writerOptions.fileSystem(fs);
+            declaredField.setAccessible(false);
+        }
+    }
+
+    @Override
+    public void writeSingleRecordToFile(RowData row) throws WriteRecordException {
+        if (recordWriter == null) {
+            nextBlock();
+        }
+
+        List<Object> recordList = new ArrayList<>();
+        int i = 0;
+        try {
+            rowConverter.toExternal(row, recordList);
+        } catch (Exception e) {
+            if (e instanceof WriteRecordException) {
+                throw (WriteRecordException) e;
+            } else {
+                throw new WriteRecordException(recordConvertDetailErrorMessage(i, row), e, i, row);
+            }
+        }
+
+        try {
+            this.recordWriter.write(
+                    NullWritable.get(), this.orcSerde.serialize(recordList, this.inspector));
+            rowsOfCurrentBlock++;
+
+            lastRow = row;
+        } catch (IOException e) {
+            String errorMessage =
+                    Hive3Util.parseErrorMsg(
+                            String.format("writer hdfs error，row:{%s}", row),
+                            ExceptionUtil.getErrorMessage(e));
+            LOG.error(errorMessage);
+            throw new WriteRecordException(errorMessage, e);
+        }
+    }
+
+    @Override
+    // todo the deviation needs to be calculated accurately
+    protected long getCurrentFileSize() {
+        return (long) (bytesWriteCounter.getLocalValue() * getDeviation());
+    }
+
+    @Override
+    protected void flushDataInternal() {
+        LOG.info(
+                "Close current orc record writer, write data size:[{}]",
+                SizeUnitType.readableFileSize(bytesWriteCounter.getLocalValue()));
+
+        try {
+            if (recordWriter != null) {
+                recordWriter.close(Reporter.NULL);
+                recordWriter = null;
+            }
+        } catch (IOException e) {
+            throw new ChunJunRuntimeException(
+                    Hive3Util.parseErrorMsg(
+                            "error to flush stream.", ExceptionUtil.getErrorMessage(e)),
+                    e);
+        }
+    }
+
+    @Override
+    protected void closeSource() {
+        try {
+            LOG.info("close:Current block writer record:" + rowsOfCurrentBlock);
+            RecordWriter rw = this.recordWriter;
+            if (rw != null) {
+                rw.close(Reporter.NULL);
+                this.recordWriter = null;
+            }
+        } catch (IOException e) {
+            throw new ChunJunRuntimeException("close stream error.", e);
+        } finally {
+            super.closeSource();
+        }
+    }
+}
---
+++ chunjun-connectors/chunjun-connector-hive3/src/main/java/com/dtstack/chunjun/connector/hive3/source/HdfsOrcInputFormat.java
@@ -0,0 +1,260 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.chunjun.connector.hive3.source;
+
+import com.dtstack.chunjun.conf.FieldConf;
+import com.dtstack.chunjun.connector.hive3.inputSplit.HdfsOrcInputSplit;
+import com.dtstack.chunjun.connector.hive3.util.Hive3Util;
+import com.dtstack.chunjun.constants.ConstantValue;
+import com.dtstack.chunjun.throwable.ChunJunRuntimeException;
+import com.dtstack.chunjun.throwable.ReadRecordException;
+
+import org.apache.flink.core.io.InputSplit;
+import org.apache.flink.table.data.GenericRowData;
+import org.apache.flink.table.data.RowData;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.ql.io.orc.OrcFile;
+import org.apache.hadoop.hive.ql.io.orc.OrcInputFormat;
+import org.apache.hadoop.hive.ql.io.orc.OrcSerde;
+import org.apache.hadoop.hive.ql.io.orc.OrcSplit;
+import org.apache.hadoop.hive.ql.io.orc.OrcStruct;
+import org.apache.hadoop.hive.serde2.objectinspector.StructField;
+import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapred.InputFormat;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.io.IOException;
+import java.security.PrivilegedAction;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/** @author liuliu 2022/3/23 */
+public class HdfsOrcInputFormat extends BaseHdfsInputFormat {
+    protected transient FileSystem fs;
+
+    protected transient String[] fullColNames;
+
+    protected transient StructObjectInspector inspector;
+
+    protected transient List<? extends StructField> fields;
+
+    protected static final String COMPLEX_FIELD_TYPE_SYMBOL_REGEX = ".*(<|>|\\{|}|[|]).*";
+
+    private final AtomicBoolean isInit = new AtomicBoolean(false);
+
+    @Override
+    protected InputSplit[] createHdfsSplit(int minNumSplits) throws IOException {
+        initHadoopJobConf();
+        // 非事务表创建分片
+        org.apache.hadoop.mapred.FileInputFormat.setInputPaths(hadoopJobConf, hdfsConf.getPath());
+        org.apache.hadoop.mapred.FileInputFormat.setInputPathFilter(
+                hadoopJobConf, HdfsPathFilter.class);
+
+        OrcInputFormat orcInputFormat = new OrcInputFormat();
+        org.apache.hadoop.mapred.InputSplit[] splits =
+                orcInputFormat.getSplits(hadoopJobConf, minNumSplits);
+
+        if (splits != null) {
+            List<HdfsOrcInputSplit> list = new ArrayList<>(splits.length);
+            int i = 0;
+            for (org.apache.hadoop.mapred.InputSplit split : splits) {
+                OrcSplit orcSplit = (OrcSplit) split;
+                if (orcSplit.getLength() > 49) {
+                    list.add(new HdfsOrcInputSplit(orcSplit, i));
+                    i++;
+                }
+            }
+            return list.toArray(new HdfsOrcInputSplit[i]);
+        }
+        return null;
+    }
+
+    @Override
+    public InputFormat<NullWritable, OrcStruct> createMapredInputFormat() {
+        return new OrcInputFormat();
+    }
+
+    @Override
+    protected void openInternal(InputSplit inputSplit) throws IOException {
+        if (super.openKerberos) {
+            ugi.doAs(
+                    (PrivilegedAction<Object>)
+                            () -> {
+                                try {
+                                    orcOpenInternal(inputSplit);
+                                } catch (IOException e) {
+                                    throw new ChunJunRuntimeException(
+                                            "failed to open orc internal", e);
+                                }
+                                return null;
+                            });
+        } else {
+            orcOpenInternal(inputSplit);
+        }
+    }
+
+    protected void orcOpenInternal(InputSplit inputSplit) throws IOException {
+        OrcSplit orcSplit = ((HdfsOrcInputSplit) inputSplit).getOrcSplit();
+        fs = FileSystem.get(hadoopJobConf);
+        init(orcSplit);
+        openOrcReader(inputSplit);
+    }
+
+    @Override
+    protected RowData nextRecordInternal(RowData rowData) throws ReadRecordException {
+        try {
+            List<FieldConf> fieldConfList = hdfsConf.getColumn();
+            GenericRowData genericRowData =
+                    new GenericRowData(Math.max(fieldConfList.size(), fullColNames.length));
+            if (fieldConfList.size() == 1
+                    && ConstantValue.STAR_SYMBOL.equals(fieldConfList.get(0).getName())) {
+
+                for (int i = 0; i < fullColNames.length; i++) {
+                    Object col = inspector.getStructFieldData(value, fields.get(i));
+                    if (col != null) {
+                        col = Hive3Util.getWritableValue(col);
+                    }
+                    genericRowData.setField(i, col);
+                }
+            } else {
+                for (int i = 0; i < fieldConfList.size(); i++) {
+                    FieldConf fieldConf = fieldConfList.get(i);
+                    Object val = null;
+                    if (fieldConf.getValue() != null) {
+                        val = fieldConf.getValue();
+                    } else if (fieldConf.getIndex() != -1) {
+                        val = inspector.getStructFieldData(value, fields.get(fieldConf.getIndex()));
+                    }
+
+                    if (val != null) {
+                        val = Hive3Util.getWritableValue(val);
+                    }
+
+                    genericRowData.setField(i, val);
+                }
+            }
+            return rowConverter.toInternal(genericRowData);
+        } catch (Exception e) {
+            throw new ReadRecordException("", e, 0, rowData);
+        }
+    }
+
+    protected List<String> parseColumnAndType(String typeStruct) {
+        List<String> cols = new ArrayList<>();
+        List<String> splits = Arrays.asList(typeStruct.split(","));
+        Iterator<String> it = splits.iterator();
+        while (it.hasNext()) {
+            StringBuilder current = new StringBuilder(it.next());
+            if (!current.toString().contains("(") && !current.toString().contains(")")) {
+                cols.add(current.toString());
+                continue;
+            }
+
+            if (current.toString().contains("(") && current.toString().contains(")")) {
+                cols.add(current.toString());
+                continue;
+            }
+
+            if (current.toString().contains("(") && !current.toString().contains(")")) {
+                while (it.hasNext()) {
+                    String next = it.next();
+                    current.append(",").append(next);
+                    if (next.contains(")")) {
+                        break;
+                    }
+                }
+
+                cols.add(current.toString());
+            }
+        }
+
+        return cols;
+    }
+
+    public void init(OrcSplit orcSplit) throws IOException {
+        try {
+            if (!isInit.get()) {
+                init(orcSplit.getPath());
+                isInit.set(true);
+            }
+        } catch (Exception e) {
+            throw new IOException("init [inspector] error", e);
+        }
+    }
+
+    private void openOrcReader(InputSplit inputSplit) throws IOException {
+        numReadCounter = getRuntimeContext().getLongCounter("numRead");
+        HdfsOrcInputSplit hdfsOrcInputSplit = (HdfsOrcInputSplit) inputSplit;
+        OrcSplit orcSplit = hdfsOrcInputSplit.getOrcSplit();
+        findCurrentPartition(orcSplit.getPath());
+        recordReader = inputFormat.getRecordReader(orcSplit, hadoopJobConf, Reporter.NULL);
+        key = recordReader.createKey();
+        value = recordReader.createValue();
+        fields = inspector.getAllStructFieldRefs();
+    }
+
+    private void init(Path path) throws Exception {
+        OrcFile.ReaderOptions readerOptions = OrcFile.readerOptions(hadoopJobConf);
+        readerOptions.filesystem(fs);
+
+        org.apache.hadoop.hive.ql.io.orc.Reader reader = OrcFile.createReader(path, readerOptions);
+        String typeStruct = reader.getObjectInspector().getTypeName();
+        // struct<id:int,name:string,age:int>
+        if (StringUtils.isEmpty(typeStruct)) {
+            throw new RuntimeException("can't retrieve type struct from " + path);
+        }
+
+        int startIndex = typeStruct.indexOf("<") + 1;
+        int endIndex = typeStruct.lastIndexOf(">");
+        typeStruct = typeStruct.substring(startIndex, endIndex);
+
+        if (typeStruct.matches(COMPLEX_FIELD_TYPE_SYMBOL_REGEX)) {
+            throw new RuntimeException(
+                    "Field types such as array, map, and struct are not supported.");
+        }
+
+        List<String> cols = parseColumnAndType(typeStruct);
+
+        fullColNames = new String[cols.size()];
+        String[] fullColTypes = new String[cols.size()];
+
+        for (int i = 0; i < cols.size(); ++i) {
+            String[] temp = cols.get(i).split(":");
+            fullColNames[i] = temp[0];
+            fullColTypes[i] = temp[1];
+        }
+
+        Properties p = new Properties();
+        p.setProperty("columns", StringUtils.join(fullColNames, ","));
+        p.setProperty("columns.types", StringUtils.join(fullColTypes, ":"));
+
+        OrcSerde orcSerde = new OrcSerde();
+        orcSerde.initialize(hadoopJobConf, p);
+
+        this.inspector = (StructObjectInspector) orcSerde.getObjectInspector();
+    }
+}
