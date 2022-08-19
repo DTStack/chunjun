71b2e44f97dc2258bb32db26910bb90b1c65856e
He Wang <wanghechn@qq.com>
2022-08-19 11:23:45 +0800

    add oceanbasecdc connector

---
+++ chunjun-connectors/chunjun-connector-oceanbasecdc/src/main/java/com/dtstack/chunjun/connector/oceanbasecdc/entity/OceanBaseCdcEventRow.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.chunjun.connector.oceanbasecdc.entity;
+
+import com.oceanbase.oms.logmessage.DataMessage;
+import com.oceanbase.oms.logmessage.LogMessage;
+
+import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OceanBaseCdcEventRow implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /** The database name in log message, would be in format 'tenant.db'. */
+    private final String database;
+
+    private final String table;
+    private final DataMessage.Record.Type type;
+
+    /** Timestamp in seconds. */
+    private final long timestamp;
+
+    private final Map<String, DataMessage.Record.Field> fieldsBefore;
+    private final Map<String, DataMessage.Record.Field> fieldsAfter;
+
+    public OceanBaseCdcEventRow(LogMessage logMessage) {
+        this.database = logMessage.getDbName();
+        this.table = logMessage.getTableName();
+        this.type = logMessage.getOpt();
+        this.timestamp = Long.parseLong(logMessage.getTimestamp());
+        this.fieldsBefore = new HashMap<>();
+        this.fieldsAfter = new HashMap<>();
+
+        for (DataMessage.Record.Field field : logMessage.getFieldList()) {
+            if (field.isPrev()) {
+                fieldsBefore.put(field.getFieldname(), field);
+            } else {
+                fieldsAfter.put(field.getFieldname(), field);
+            }
+        }
+    }
+
+    /**
+     * Convert field map to 'field name'->'field value (string)'
+     *
+     * @param fields Original field map.
+     * @return Field value map.
+     */
+    @SuppressWarnings("rawtypes")
+    public static Map toValueMap(Map<String, DataMessage.Record.Field> fields) {
+        return fields.entrySet().stream()
+                .collect(
+                        Collectors.toMap(
+                                Map.Entry::getKey,
+                                entry ->
+                                        entry.getValue()
+                                                .getValue()
+                                                .toString(StandardCharsets.UTF_8.name())));
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public DataMessage.Record.Type getType() {
+        return type;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public Map<String, DataMessage.Record.Field> getFieldsBefore() {
+        return fieldsBefore;
+    }
+
+    public Map<String, DataMessage.Record.Field> getFieldsAfter() {
+        return fieldsAfter;
+    }
+}
---
+++ chunjun-connectors/chunjun-connector-oceanbasecdc/src/main/java/com/dtstack/chunjun/connector/oceanbasecdc/options/OceanBaseCdcOptions.java
@@ -0,0 +1,96 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.chunjun.connector.oceanbasecdc.options;
+
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ConfigOptions;
+
+public class OceanBaseCdcOptions {
+
+    public static final ConfigOption<String> LOG_PROXY_HOST =
+            ConfigOptions.key("log-proxy-host")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription("Hostname or IP address of OceanBase log proxy service.");
+
+    public static final ConfigOption<Integer> LOG_PROXY_PORT =
+            ConfigOptions.key("log-proxy-port")
+                    .intType()
+                    .noDefaultValue()
+                    .withDescription("Port number of OceanBase log proxy service.");
+
+    public static final ConfigOption<String> USERNAME =
+            ConfigOptions.key("username")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription("Username to be used when connecting to OceanBase.");
+
+    public static final ConfigOption<String> PASSWORD =
+            ConfigOptions.key("password")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription("Password to be used when connecting to OceanBase.");
+
+    public static final ConfigOption<String> TABLE_WHITELIST =
+            ConfigOptions.key("table-whitelist")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "Table whitelist in format 'tenant'.'db'.'table', will use 'fnmatch' to match the three parts of it, multiple values should be separated by '|'.");
+
+    public static final ConfigOption<Long> STARTUP_TIMESTAMP =
+            ConfigOptions.key("start-timestamp")
+                    .longType()
+                    .noDefaultValue()
+                    .withDescription("Startup timestamp in seconds.");
+
+    public static final ConfigOption<String> RS_LIST =
+            ConfigOptions.key("rootservice-list")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "The semicolon-separated list of root servers in format `ip:rpc_port:sql_port`, corresponding to the parameter 'rootservice_list' in the OceanBase CE.");
+
+    public static final ConfigOption<String> CONFIG_URL =
+            ConfigOptions.key("config-url")
+                    .stringType()
+                    .noDefaultValue()
+                    .withDescription(
+                            "The url used to get root servers list, corresponding to the parameter 'obconfig_url' in the OceanBase EE.");
+
+    public static final ConfigOption<String> TIMEZONE =
+            ConfigOptions.key("timezone")
+                    .stringType()
+                    .defaultValue("+08:00")
+                    .withDescription("The timezone in database server.");
+
+    public static final ConfigOption<String> WORKING_MODE =
+            ConfigOptions.key("working-mode")
+                    .stringType()
+                    .defaultValue("memory")
+                    .withDescription(
+                            "The working mode of 'obcdc', can be `memory` or `storage` (supported from `obcdc` 3.1.3).");
+
+    public static final ConfigOption<String> CAT =
+            ConfigOptions.key("cat")
+                    .stringType()
+                    .defaultValue("insert, delete, update")
+                    .withDescription(
+                            "Operation types need to be captured, can be one or more of 'insert' 'delete' and 'update', separated by commas.");
+}
