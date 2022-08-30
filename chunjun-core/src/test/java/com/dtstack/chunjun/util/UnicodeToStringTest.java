509d7b2780eaca0629371aa21511226bd3ed7e21
momisabuilder <41328062+momisabuilder@users.noreply.github.com>
2022-08-30 17:14:22 +0800

    [hotfix-#1195]change the code position to support oracle nchar、nvarch… (#1196)
    [hotfix-#1195]change the code position to support oracle nchar、nvarchar2 chinese value

--- chunjun-core/src/test/java/com/dtstack/chunjun/util/UnicodeToStringTest.java
+++ chunjun-core/src/test/java/com/dtstack/chunjun/util/UnicodeToStringTest.java
@@ -18,19 +18,15 @@
 
 package com.dtstack.chunjun.util;
 
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @author liuche
  * @date 2022/8/26 14:39
  */
 public class UnicodeToStringTest {
-
-    public static final Pattern pattern = Pattern.compile("(\\\\u(\\w{4}))");
 
     @Test
     public void testUnicodeToString() {
@@ -39,18 +35,8 @@ public class UnicodeToStringTest {
         if (str.startsWith("UNISTR('") && str.endsWith("')")) {
             String substring = str.substring(8, str.length() - 2);
             String replace = substring.replace("\\", "\\u");
-            str = unicodeToString(replace);
+            str = StringEscapeUtils.unescapeJava(replace);
         }
         Assert.assertEquals(str, "大海");
-    }
-
-    public static String unicodeToString(String str) {
-        Matcher matcher = pattern.matcher(str);
-        char ch;
-        while (matcher.find()) {
-            ch = (char) Integer.parseInt(matcher.group(2), 16);
-            str = str.replace(matcher.group(1), String.valueOf(ch));
-        }
-        return str;
     }
 }
