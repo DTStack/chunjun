0f498d8859868af1163d4529d56b437745de929c
高阿四 <gaoasi@sensoro.com>
2022-10-28 17:46:41 +0800

    [feat] [chunjun-connector-stream,chunjun-connector-elasticsearch-base,chunjun-connector-jdbs-base] enrich sql type data type,add [ROW,ARRAY,MAP,MULTISET] data type,only support sql type
    [test] [chunjun-examples/sql/[stream,elasticsearch7,postgresql], add test file test [ROW,ARRAY,MAP] data type

---
+++ chunjun-examples/sql/elasticsearch7/insert_row_array_map_data_type.sql
@@ -0,0 +1,52 @@
+CREATE TABLE source
+(
+    id        INT,
+    name      STRING,
+    money     DECIMAL(32, 2),
+    dateone   timestamp,
+    age       bigint,
+    datethree timestamp,
+    datesix   timestamp(6),
+    datenigth timestamp(9),
+    dtdate    date,
+    dttime    time,
+    `row` ROW(id int,name varchar),
+    plateText ARRAY<varchar>,
+    pt MAP<varchar,int>
+) WITH (
+      'connector' = 'stream-x',
+      'number-of-rows' = '10', -- 输入条数，默认无限
+      'rows-per-second' = '1' -- 每秒输入条数，默认不限制
+      );
+
+CREATE TABLE es7_sink
+(
+    id        INT,
+    name      STRING,
+    money     DECIMAL(32, 2),
+    dateone   timestamp,
+    age       bigint,
+    datethree timestamp,
+    datesix   timestamp(6),
+    datenigth timestamp(9),
+    dtdate    date,
+    dttime    time,
+    `row` ROW(id int,name varchar),
+    plateText ARRAY<varchar>,
+    pt MAP<varchar,int>
+)
+    WITH (
+        'connector' = 'elasticsearch7-x',
+        'hosts' = '127.0.0.1:9200',
+        'username' = 'elastic',
+        'password' = 'elastic',
+        'index' = 'students_4',
+        'client.connect-timeout' = '10000',
+        'security.ssl-keystore-file'='ca.crt',
+        'security.ssl-keystore-password'='',
+        'security.ssl-type'='ca'
+        );
+
+
+insert into es7_sink
+select * from source;
