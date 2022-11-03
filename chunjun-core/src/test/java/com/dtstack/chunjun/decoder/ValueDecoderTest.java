85dc860c739effb1d87d0a338cb797ca4a37d9c9
liumengkai <1623857502@qq.com>
2022-11-03 15:21:12 +0800

    [feature-#1353][kafka-connector] add valuedecoder Test

---
+++ chunjun-core/src/test/java/com/dtstack/chunjun/decoder/ValueDecoderTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.chunjun.decoder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ValueDecoderTest {
+
+    private ValueDecoder valueDecoder;
+
+    @BeforeEach
+    void setUp() {
+        valueDecoder = new ValueDecoder();
+    }
+
+    @Test
+    @DisplayName("Should return a map with the message")
+    void decodeShouldReturnAMapWithTheMessage() {
+        final String message = "Hello World";
+        final Map<String, Object> result = valueDecoder.decode(message);
+        assertEquals(Collections.singletonMap("value", message), result);
+    }
+}
