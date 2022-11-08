67604a0f05dbb4b998a45cc0a9010bf1e8c64cf2
FlechazoW <35768015+FlechazoW@users.noreply.github.com>
2022-11-08 09:51:09 +0800

    [Improve][License] Add apache-2.0 license check. (#1386)

--- chunjun-connectors/chunjun-connector-vertica11/src/main/java/com/dtstack/chunjun/connector/vertica11/dialect/Vertica11Dialect.java
+++ chunjun-connectors/chunjun-connector-vertica11/src/main/java/com/dtstack/chunjun/connector/vertica11/dialect/Vertica11Dialect.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.dtstack.chunjun.connector.vertica11.dialect;
 
 import com.dtstack.chunjun.conf.ChunJunCommonConf;
