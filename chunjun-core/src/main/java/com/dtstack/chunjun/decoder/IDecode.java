d981ced0a8d01d6ad4263f76fb2c26d73e424a7e
FlechazoW <35768015+FlechazoW@users.noreply.github.com>
2022-11-08 09:51:09 +0800

    [Improve][License] Add apache-2.0 license check. (#1386)
    (cherry picked from commit 67604a0f05dbb4b998a45cc0a9010bf1e8c64cf2)

--- chunjun-core/src/main/java/com/dtstack/chunjun/decoder/IDecode.java
+++ chunjun-core/src/main/java/com/dtstack/chunjun/decoder/IDecode.java
@@ -6,24 +6,20 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * <p>
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.dtstack.chunjun.decoder;
 
 import java.util.Map;
 
-/**
- * Date: 2019/11/21 Company: www.dtstack.com
- *
- * @author tudou
- */
 public interface IDecode {
 
     /**
