cfd8e68ca67767c16d195fd0bb228018ecd49f23
libailin <bailin_li@163.com>
2023-06-09 11:33:06 +0800

    [Feature-#1672][connector][gbase] add gbase connector (#1716)

---
+++ chunjun-connectors/chunjun-connector-gbase/src/main/java/com/dtstack/chunjun/connector/gbase/source/GbaseSourceFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.chunjun.connector.gbase.source;
+
+import com.dtstack.chunjun.config.SyncConfig;
+import com.dtstack.chunjun.connector.gbase.dialect.GbaseDialect;
+import com.dtstack.chunjun.connector.jdbc.source.JdbcSourceFactory;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class GbaseSourceFactory extends JdbcSourceFactory {
+
+    public GbaseSourceFactory(SyncConfig syncConfig, StreamExecutionEnvironment env) {
+        super(syncConfig, env, new GbaseDialect());
+        // 避免result.next阻塞
+        if (jdbcConfig.isPolling()
+                && StringUtils.isEmpty(jdbcConfig.getStartLocation())
+                && jdbcConfig.getFetchSize() == 0) {
+            jdbcConfig.setFetchSize(1000);
+        }
+    }
+}
---
+++ chunjun-connectors/chunjun-connector-gbase/src/main/java/com/dtstack/chunjun/connector/gbase/table/GbaseDynamicTableFactory.java
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dtstack.chunjun.connector.gbase.table;
+
+import com.dtstack.chunjun.connector.gbase.dialect.GbaseDialect;
+import com.dtstack.chunjun.connector.jdbc.dialect.JdbcDialect;
+import com.dtstack.chunjun.connector.jdbc.table.JdbcDynamicTableFactory;
+
+public class GbaseDynamicTableFactory extends JdbcDynamicTableFactory {
+
+    private static final String IDENTIFIER = "gbase-x";
+
+    @Override
+    public String factoryIdentifier() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    protected JdbcDialect getDialect() {
+        return new GbaseDialect();
+    }
+}
